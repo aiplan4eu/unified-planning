a10ad4d91b8b6279d0e6b13c2342624d1b0107d1
Alessandro Valentini <alvalentini@fbk.eu>
2021-05-12 14:48:30 +0200

    Added Copyright

--- upf/test/test_solve_with_heuristic.py
+++ upf/test/test_solve_with_heuristic.py
@@ -1,3 +1,17 @@
+# Copyright 2021 AIPlan4EU project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import upf
 from upf.test import TestCase, main
 from upf.test import generate_simple_problem
@@ -17,7 +31,7 @@ class TestSolveWithHeuristic(TestCase):
         self.size = 14
         self.problem = generate_simple_problem(self.size)
 
-    def test_cppyplanner(self):
+    def test_cppplanner(self):
         with upf.Planner('upf_cppplanner') as p:
             plan = p.solve(self.problem, GoalCounterHeuristic(self.size, self.problem))
             self.assertEqual(len(plan), self.size*3-1)
