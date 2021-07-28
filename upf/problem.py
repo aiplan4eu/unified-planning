6207f534d11efbdc2ed50b5e74d97095ff45b8bc
Alessandro Valentini <alvalentini@fbk.eu>
2021-07-28 17:26:16 +0200

    Minor fix

--- upf/problem.py
+++ upf/problem.py
@@ -229,9 +229,79 @@ class Problem:
         else:
             raise UPFProblemDefinitionError('Initial value not set!')
 
+    def domain_size(self, typename: upf.typing.Type) -> Optional[int]:
+        """Returns the domain size of the given type."""
+        if typename.is_bool_type():
+            return 2
+        elif typename.is_user_type():
+            return len(self.objects(typename))
+        elif typename.is_real_type():
+            lb = typename.lower_bound() # type: ignore
+            ub = typename.upper_bound() # type: ignore
+            if lb is not None and lb == ub:
+                return 1
+        elif typename.is_int_type():
+            lb = typename.lower_bound() # type: ignore
+            ub = typename.upper_bound() # type: ignore
+            if lb is not None and ub is not None:
+                return ub - lb
+        return None
+
+    def domain_item(self, typename: upf.typing.Type, idx: int) -> FNode:
+        """Returns the ith domain item of the given type."""
+        if typename.is_bool_type():
+            return self._env.expression_manager.Bool(idx == 0)
+        elif typename.is_user_type():
+            return self._env.expression_manager.ObjectExp(self.objects(typename)[idx])
+        elif typename.is_real_type():
+            lb = typename.lower_bound() # type: ignore
+            ub = typename.upper_bound() # type: ignore
+            assert (lb is not None and lb == ub)
+            return self._env.expression_manager.Real(lb)
+        elif typename.is_int_type():
+            lb = typename.lower_bound() # type: ignore
+            ub = typename.upper_bound() # type: ignore
+            assert (lb is not None and ub is not None)
+            return self._env.expression_manager.Int(lb + idx)
+        else:
+            raise
+
+    def get_ith_fluent_exp(self, fluent: upf.Fluent, domain_sizes: List[int], idx: int) -> FNode:
+        """Returns the ith ground fluent expression."""
+        quot = idx
+        rem = 0
+        actual_parameters = []
+        for i in range(fluent.arity()):
+            ds = domain_sizes[i];
+            rem = quot % ds
+            quot //= ds
+            v = self.domain_item(fluent.signature()[i], rem)
+            actual_parameters.append(v)
+        return fluent(*actual_parameters)
+
     def initial_values(self) -> Dict[FNode, FNode]:
         """Gets the initial value of the fluents."""
-        return self._initial_value
+        res = self._initial_value
+        for f in self._fluents.values():
+            if f.arity() == 0:
+                f_exp = self._env.expression_manager.FluentExp(f)
+                res[f_exp] = self.initial_value(f_exp)
+            else:
+                ground_size = 1
+                domain_sizes = []
+                is_groundable = True
+                for p in f.signature():
+                    ds = self.domain_size(p)
+                    if ds is None:
+                        is_groundable = False
+                        break
+                    domain_sizes.append(ds)
+                    ground_size *= ds
+                if is_groundable:
+                    for i in range(ground_size):
+                        f_exp = self.get_ith_fluent_exp(f, domain_sizes, i)
+                        res[f_exp] = self.initial_value(f_exp)
+        return res
 
     def add_goal(self, goal: Union[FNode, upf.Fluent, bool]):
         """Adds a goal."""
