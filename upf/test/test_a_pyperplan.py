2138c13fdc5a6d133e4a6125cd8ab05ecc8b8558
Framba-Luca <framba@fbk.eu>
2021-09-03 13:09:58 +0200

    Removed file parsing.

--- upf/test/test_a_pyperplan.py
+++ upf/test/test_a_pyperplan.py
@@ -24,6 +24,33 @@ class TestPyperplan(TestCase):
         self.env = get_env()
 
     def test_pyperplan(self):
+        Location = UserType('location')
+        robot_at = upf.Fluent('robot_at', BoolType(), [Location])
+        move = upf.Action('move', l_from=Location, l_to=Location)
+        l_from = move.parameter('l_from')
+        l_to = move.parameter('l_to')
+        move.add_precondition(robot_at(l_from))
+        move.add_effect(robot_at(l_from), False)
+        move.add_effect(robot_at(l_to), True)
+        l1 = upf.Object('l1', Location)
+        l2 = upf.Object('l2', Location)
+        problem = upf.Problem('robot')
+        problem.add_fluent(robot_at)
+        problem.add_action(move)
+        problem.add_object(l1)
+        problem.add_object(l2)
+        problem.set_initial_value(robot_at(l1), True)
+        problem.set_initial_value(robot_at(l2), False)
+        problem.add_goal(robot_at(l2))
+        plan = upf.SequentialPlan([upf.ActionInstance(move, [ObjectExp(l1), ObjectExp(l2)])])
+        self.env.factory.add_solver('pyperplan', 'upf_pyperplan', 'SolverImpl')
+
+        with OneshotPlanner(name='pyperplan') as planner:
+            self.assertNotEqual(planner, None)
+            new_plan = planner.solve(problem)
+            self.assertEqual(str(plan), str(new_plan))
+
+    def test_pyperplan_2(self):
         x = upf.Fluent('x')
         y = upf.Fluent('y')
         a = upf.Action('a')
