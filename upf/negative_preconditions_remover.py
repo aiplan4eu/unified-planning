412b382f5bfa0c0560af91b1818445e5ef5d308f
Framba-Luca <framba@fbk.eu>
2021-09-13 17:28:58 +0200

    Implemented map from Fluent to Fluent negation created with preconditions and goal changes, still missing init and effect changes.

--- upf/negative_preconditions_remover.py
+++ upf/negative_preconditions_remover.py
@@ -15,14 +15,37 @@
 """This module defines the negative preconditions remover class."""
 
 from collections import OrderedDict
+from upf.fluent import Fluent
 from upf.plan import SequentialPlan, ActionInstance
 from upf.problem import Problem
 from upf.action import Action
-from upf.effect import Effect
 from upf.fnode import FNode
-from upf.simplifier import Simplifier
+from upf.walkers.identitydag import IdentityDagWalker
+from upf.exceptions import UPFExpressionDefinitionError
 from typing import Iterable, List, Dict, Tuple
 from itertools import chain, combinations
+
+
+class NegativeFluentRemover(IdentityDagWalker):
+    def __init__(self, env):
+        self._env = env
+        IdentityDagWalker.__init__(self, self._env)
+        self._fluent_mapping: Dict[Fluent, Fluent] = {}
+
+    def remove_negative_fluents(self, expression:FNode) -> FNode:
+        return self.walk(expression)
+
+    def walk_not(self, expression: FNode, args: List[FNode], **kwargs) -> FNode:
+        assert len(args) == 1
+        if not args[0].is_fluent_exp():
+            raise UPFExpressionDefinitionError(f"Expression: {expression} is not in NNF.")
+        neg_fluent = self._fluent_mapping.get(args[0].fluent(), None)
+        if neg_fluent is not None:
+            return self._env.expression_manager.FluentExp(neg_fluent, tuple(args[0].args()))
+        f = args[0].fluent()
+        nf = Fluent(f.name()+"__negated__", f.type(), f.signature(), f._env)
+        self._fluent_mapping[f] = nf
+        return self._env.expression_manager.FluentExp(nf, tuple(args[0].args()))
 
 
 class NegativePreconditionsRemover():
@@ -37,10 +60,9 @@ class NegativePreconditionsRemover():
         self._problem = problem
         self._env = problem.env
         self._positive_problem = None
-        self._fluent_mapping: Dict[FNode, FNode] = {}
         self._count = 0
         #NOTE no simplification are made. But it's possible to add them in key points
-        self._simplifier = Simplifier(self._env)
+        self._fluent_remover = NegativeFluentRemover(self._env)
 
     def get_rewritten_problem(self) -> Problem:
         '''Creates a problem that is a copy of the original problem
@@ -49,17 +71,34 @@ class NegativePreconditionsRemover():
         if self._positive_problem is not None:
             return self._positive_problem
         #NOTE that a different environment might be needed when multy-threading
-        new_problem = self._create_problem_copy_without_actions_and_goals()
-        for a in self._problem.actions().values():
-            na = self._action_without_negative_preconditions(a)
-            new_problem.add_action(na)
-        for g in self._problem.goals():
-            ng = self._expression_quantifier_remover.remove_quantifiers(g, self._problem)
-            new_problem.add_goal(ng)
+        new_problem = self._create_problem_copy_without_actions_init_and_goals()
+        self._modify_actions_and_goals(new_problem)
         self._positive_problem = new_problem
         return new_problem
 
-    def _create_problem_copy_without_actions_and_goals(self):
+    def _modify_actions_and_goals(self, new_problem):
+        name_action_map: Dict[str, Action] = {}
+        for name, action in self._problem.actions().items():
+            new_action = Action(name, OrderedDict((ap.name(), ap.type()) for ap in action.parameters()), self._env)
+            for p in action.preconditions():
+                np = self._fluent_remover.remove_negative_fluents(p)
+                new_action.add_precondition(np)
+            name_action_map[name] = new_action
+        for g in self._problem.goals():
+            ng = self._fluent_remover.remove_negative_fluents(g)
+            new_problem.add_goal(ng)
+        fluent_mapping = self._fluent_remover._fluent_mapping
+        for fl, v in self._problem.initial_values().items():
+            fneg = fluent_mapping.get(fl.fluent(), None)
+            if fneg is not None:
+                #HERE
+            new_problem.set_initial_value(fl, v)
+
+
+
+
+
+    def _create_problem_copy_without_actions_init_and_goals(self):
         '''Creates the shallow copy of a problem, without adding the actions
         with quantifiers and by pushing them to the stack
         '''
@@ -68,17 +107,17 @@ class NegativePreconditionsRemover():
             new_problem.add_fluent(f)
         for o in self._problem.all_objects():
             new_problem.add_object(o)
-        for fl, v in self._problem.initial_values().items():
-            new_problem.set_initial_value(fl, v)
         return new_problem
 
     def _action_without_negative_preconditions(self, action) -> Action:
         new_action = Action(action.name(), OrderedDict((ap.name(), ap.type()) for ap in action.parameters()), self._env)
-#HERE
+
         for p in action.preconditions():
-            np = self._expression_quantifier_remover.remove_quantifiers(p, self._problem)
+            np = self._fluent_remover.remove_negative_fluents(p)
             new_action.add_precondition(np)
         for e in action.effects():
+            if e.is_conditional():
+                raise
             new_action._add_effect_instance(e)
         return new_action
 
