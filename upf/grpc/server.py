eed69c9a35e7991d700cf0ad2cbf90000339cfad
Uwe Köckemann <uweewu@gmail.com>
2021-09-28 14:31:10 +0200

    Trying to make grpc optional

--- upf/grpc/server.py
+++ upf/grpc/server.py
@@ -1,31 +1,35 @@
-from concurrent import futures
-import grpc
+try:
+    from concurrent import futures
+    import grpc
 
-from upf.shortcuts import *
-import upf.grpc.upf_pb2_grpc as upf_pb2_grpc
-from upf.grpc.factory import ProtoFactory
+    from upf.shortcuts import *
+    import upf.grpc.upf_pb2_grpc as upf_pb2_grpc
+    from upf.grpc.factory import ProtoFactory
 
-class UpfGrpcServer(upf_pb2_grpc.UpfServicer):
-    def __init__(self, port):
-        self.server = None
-        self.port = port
-        self.factory = ProtoFactory()
 
-    def plan(self, request, context):
-        problem = self.factory.message2problem(request)
-        
-        with OneshotPlanner(name='tamer', params={'weight': 0.8}) as planner:
-            plan = planner.solve(problem)
-            answer = self.factory.plan2message(plan)
-            return answer
-        return None
+    class UpfGrpcServer(upf_pb2_grpc.UpfServicer):
+        def __init__(self, port):
+            self.server = None
+            self.port = port
+            self.factory = ProtoFactory()
 
-    def start(self):
-        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        upf_pb2_grpc.add_UpfServicer_to_server(
-            self, self.server)
-        self.server.add_insecure_port('0.0.0.0:%d' % self.port)
-        self.server.start()
+        def plan(self, request, context):
+            problem = self.factory.message2problem(request)
 
-    def wait_for_termination(self):
-        self.server.wait_for_termination()
+            with OneshotPlanner(name='tamer', params={'weight': 0.8}) as planner:
+                plan = planner.solve(problem)
+                answer = self.factory.plan2message(plan)
+                return answer
+            return None
+
+        def start(self):
+            self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+            upf_pb2_grpc.add_UpfServicer_to_server(
+                self, self.server)
+            self.server.add_insecure_port('0.0.0.0:%d' % self.port)
+            self.server.start()
+
+        def wait_for_termination(self):
+            self.server.wait_for_termination()
+except ImportError:
+    print("grpc module not found")
