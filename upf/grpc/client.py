83ad1a48daab520f4908a0b4bde069a638779abc
Uwe Köckemann <uweewu@gmail.com>
2021-09-28 14:31:10 +0200

    Trying to make grpc optional

--- upf/grpc/client.py
+++ upf/grpc/client.py
@@ -1,21 +1,24 @@
-import grpc
-import upf.grpc.upf_pb2_grpc as upf_pb2_grpc
-from upf.grpc.factory import ProtoFactory
+try:
+    import grpc
+    import upf.grpc.upf_pb2_grpc as upf_pb2_grpc
+    from upf.grpc.factory import ProtoFactory
 
 
-class UpfGrpcClient:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.factory = ProtoFactory()
+    class UpfGrpcClient:
+        def __init__(self, host, port):
+            self.host = host
+            self.port = port
+            self.factory = ProtoFactory()
 
-    def __call__(self, problem):
-        with grpc.insecure_channel('%s:%d' % (self.host, self.port)) as channel:
-            stub = upf_pb2_grpc.UpfStub(channel)
-            req = self.factory.problem2message(problem)
-            self.factory.message2problem(req) # TODO: Workaround to assure that fluent map is populated
+        def __call__(self, problem):
+            with grpc.insecure_channel('%s:%d' % (self.host, self.port)) as channel:
+                stub = upf_pb2_grpc.UpfStub(channel)
+                req = self.factory.problem2message(problem)
+                self.factory.message2problem(req) # TODO: Workaround to assure that fluent map is populated
 
-            answer = stub.plan(req)
+                answer = stub.plan(req)
 
-            r = self.factory.message2plan(answer)
-            return r
+                r = self.factory.message2plan(answer)
+                return r
+except ImportError:
+    print("grpc module not found")
