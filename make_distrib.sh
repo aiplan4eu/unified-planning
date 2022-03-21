eb0d1dfd41387f65f3e8c704e6f0bf447be2a99f
Andrea Micheli <amicheli@fbk.eu>
2022-03-21 10:49:54 +0100

    Fixed deployment on PyPI

--- make_distrib.sh
+++ make_distrib.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Replace version information
-# See: https://git-scm.com/docs/git-describe
-# E.g. v0.5.1-4-g49a49f2-wip
-#      * 4 commits after tag v0.5.1
-#      * Latest commit "49a49f2"
-# we get v0.5.1.dev4
-#
-DEV_VERSION=`git describe | sed  's/v\(.*\)-\(.*\)-\(.*\)/\2/'`
-sed -i "s/\"dev\", 1/\"dev\", $DEV_VERSION/" unified_planning/__init__.py
-
 # Create package files
 python3 setup.py sdist --format=gztar
 
