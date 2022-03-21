72fc1e3b24f8f0308b03ffad285371b7ca999f6d
Andrea Micheli <amicheli@fbk.eu>
2022-03-21 10:44:02 +0100

    Fixed deployment on PyPI

--- make_distrib.sh
+++ make_distrib.sh
@@ -8,7 +8,7 @@
 # we get v0.5.1.dev4
 #
 DEV_VERSION=`git describe | sed  's/v\(.*\)-\(.*\)-\(.*\)/\2/'`
-sed -i "s/\"dev\", 1/\"dev\", $DEV_VERSION/" pysmt/__init__.py
+sed -i "s/\"dev\", 1/\"dev\", $DEV_VERSION/" unified_planning/__init__.py
 
 # Create package files
 python3 setup.py sdist --format=gztar
