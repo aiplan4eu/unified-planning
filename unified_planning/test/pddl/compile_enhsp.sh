2b09fce321ed6a1d6df6726372fb5628481799a6
Alessandro Valentini <alvalentini@fbk.eu>
2022-04-07 17:00:37 +0200

    Removed custom enhsp compile script

--- unified_planning/test/pddl/compile_enhsp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir out
-javac -d out -classpath "libs/*" src/planners/*.java src/*.java
-jar --create --file enhsp.jar --manifest manifest.mf -C out/ .
-mkdir enhsp-dist
-cp -r libs/ enhsp-dist/
-cp enhsp.jar enhsp-dist/
