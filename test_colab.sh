5f31f71584a51e09ea4f041bc53f18f82ab5c101
Framba-Luca <framba@fbk.eu>
2022-05-05 17:30:03 +0200

    Improved output cosmetic and moved notebook outside of unified_planning package

--- test_colab.sh
+++ test_colab.sh
@@ -4,8 +4,8 @@ boi="# begin of installation"
 eoi="# end of installation"
 for colab_file in ./unified_planning/notebook/*.ipynb ; do
     file_name="${colab_file%.ipynb}"
-    jupyter nbconvert --to python ${colab_file}
     python_file="${file_name}.py"
+    jupyter nbconvert --to python ${colab_file} --output ${python_file}
     file_tested_name="${file_name}_rewritten.py"
     trim_rows="false"
     touch ${file_tested_name}
@@ -22,15 +22,15 @@ for colab_file in ./unified_planning/notebook/*.ipynb ; do
             echo "${line}" >> ${file_tested_name}
         fi
     done < "${python_file}"
-    echo "Testing ${file_tested_name}"
-    output="$( { ipython3 ${file_tested_name}; } 2>&1 )"
+    echo -n "  -> Testing ${file_tested_name}..."
+    output=`ipython3 ${file_tested_name} 2>&1`
     if [ ! $? == 0 ]; then
         rm ./unified_planning/notebook/*.py
-        echo "Testing ${file_tested_name} FAILED"
-        echo ${output}
+        echo " FAILED for the following reason:"
+        echo "${output}"
         exit 1
     fi
-    echo "Testing ${file_tested_name} DONE"
+    echo " DONE!"
 done
 rm ./unified_planning/notebook/*.py
 exit 0
