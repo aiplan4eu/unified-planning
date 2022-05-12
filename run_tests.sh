8bc30c8eb02098b4bba13f535c38b02580db1d37
Arthur Bit-Monnot <abitmonnot@laas.fr>
2022-05-12 13:30:59 +0200

    [ci] Prevent pytest to look into modules that require dependencies not available on Windows.
    Note that these directories do not contain any test.

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,8 @@
 
 python3 -m pytest \
   --cov=unified_planning --cov-report=xml \
-  --doctest-modules --ignore=unified_planning/grpc/generated \
+  --doctest-modules \
+  --ignore=unified_planning/grpc/generated \
+  --ignore=unified_planning/interop \
+  --ignore=unified_planning/solvers \
   unified_planning
