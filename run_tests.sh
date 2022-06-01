2abd76f2dc532d4923321bcc13afdddc09463b7d
Alessandro Valentini <alvalentini@fbk.eu>
2022-06-01 17:18:12 +0200

    Small fix

--- run_tests.sh
+++ run_tests.sh
@@ -6,5 +6,5 @@ python3 -m pytest \
   --doctest-modules \
   --ignore=unified_planning/grpc/generated \
   --ignore=unified_planning/interop \
-  --ignore=unified_planning/solvers \
+  --ignore=unified_planning/engines \
   unified_planning
