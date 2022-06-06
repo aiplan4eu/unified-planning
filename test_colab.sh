75f4b829a42cf2b04fffd66d0025ff1a01093e42
Alessandro Valentini <alvalentini@fbk.eu>
2022-06-06 14:34:10 +0200

    Fix notebook

--- test_colab.sh
+++ test_colab.sh
@@ -34,9 +34,9 @@ for colab_file in ./notebooks/*.ipynb ; do
         rm ./notebooks/*.py
         echo " FAILED for the following reason:"
         echo "${output}"
-        exit 1
+    else
+        echo " DONE!"
     fi
-    echo " DONE!"
 done
 rm ./notebooks/*.py
 exit 0
