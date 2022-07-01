bb48bbc46faf45620d3a7d0bc9c2ed0b22b75907
Luca Framba <framba@fbk.eu>
2022-07-01 15:13:26 +0200

    Initial commit

---
+++ docs/code_snippets/code_in_readme.py
@@ -0,0 +1,17 @@
+from unified_planning.shortcuts import *
+
+x = Fluent('x')
+
+a = InstantaneousAction('a')
+a.add_precondition(Not(x))
+a.add_effect(x, True)
+
+problem = Problem('basic')
+problem.add_fluent(x)
+problem.add_action(a)
+problem.set_initial_value(x, False)
+problem.add_goal(x)
+
+with OneshotPlanner(problem_kind=problem.kind) as planner:
+    plan = planner.solve(problem)
+    print(plan)
