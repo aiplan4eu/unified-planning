01406197345c260dcfe8dfedec94ee6bd2920c42
Alessandro Valentini <alvalentini@fbk.eu>
2022-07-04 18:29:59 +0200

    Fixed tests

--- scripts/test_code_snippets.sh
+++ scripts/test_code_snippets.sh
@@ -4,6 +4,8 @@ SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd ${SCRIPTS_DIR}/../
 
+export PYTHONPATH="${SCRIPTS_DIR}/../:$PYTHONPATH"
+
 for python_file in ./docs/code_snippets/*.py ; do
     python_basename_file=$(basename ${python_file})
     echo -n "  -> Testing ${python_basename_file}..."
