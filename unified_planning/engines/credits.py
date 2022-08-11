683e51b57b520d6e68a6d1fdc79346bce16197c3
Luca Framba <framba@fbk.eu>
2022-08-11 18:30:05 +0200

    ReDocumented till OverSubscriptionPlanner

--- unified_planning/engines/credits.py
+++ unified_planning/engines/credits.py
@@ -29,12 +29,11 @@ class Credits:
 
     def write_credits(self, stream: IO[str], full_credits: bool = False):
         """
-        Writes those credits on the given IO[str]; based on the flag 'full_credits'
+        Writes those credits on the given `IO[str]`; based on the flag `full_credits`
         discriminates if the user wants a long version or a short version.
 
-        :param stream: The IO[str] stream on which the credits are written.
+        :param stream: The `IO[str]` stream on which the credits are written.
         :param full_credits: Flag deciding if the user wants long or short credits.
-        :return: None.
         """
         stream.write(f"  * Engine name: {self.name}\n  * Developers:  {self.author}\n")
         if not full_credits:
