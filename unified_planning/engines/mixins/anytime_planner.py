5ef040c817cadd7db611f300a3f45a7d14284767
Alessandro Valentini <alvalentini@fbk.eu>
2022-09-02 11:54:02 +0200

    Minor renaming

--- unified_planning/engines/mixins/anytime_planner.py
+++ unified_planning/engines/mixins/anytime_planner.py
@@ -22,39 +22,40 @@ class AnytimePlannerMixin:
     def is_anytime_planner() -> bool:
         return True
 
-    def solve(
-        self,
-        problem: "up.model.AbstractProblem",
-        timeout: Optional[float] = None,
-        output_stream: Optional[IO[str]] = None,
-    ) -> Iterator["up.engines.results.PlanGenerationResult"]:
-        """This method takes a up.model.AbstractProblem and returns an iterator of up.engines.results.PlanGenerationResult,
-        which contains information about the solution to the problem given by the planner.
-
-        :param problem: is the up.model.AbstractProblem to solve.
-        :param timeout: is the time in seconds that the planner has at max to solve the problem, defaults to None.
-        :param output_stream: is a stream of strings where the planner writes his
-        output (and also errors) while the planner is solving the problem, defaults to None
-        :return: an iterator of up.engines.results.PlanGenerationResult created by the planner.
-
-        The only required parameter is "problem" but the planner should warn the user if timeout or
-        output_stream are not None and the planner ignores them."""
-        assert isinstance(self, up.engines.engine.Engine)
-        if not self.supports(problem.kind):
-            raise up.exceptions.UPUsageError(
-                f"{self.name} cannot solve this kind of problem!"
-            )
-        for res in self._solve(problem, timeout, output_stream):
-            yield res
-
-    def _solve(
+    def get_solutions(
         self,
         problem: "up.model.AbstractProblem",
         timeout: Optional[float] = None,
         output_stream: Optional[IO[str]] = None,
     ) -> Iterator["up.engines.results.PlanGenerationResult"]:
         """
-        Method called by the AnytimePlannerMixin.solve method that has to be implemented
+        This method takes a `AbstractProblem` and returns an iterator of `PlanGenerationResult`,
+        which contains information about the solution to the problem given by the planner.
+
+        :param problem: is the `AbstractProblem` to solve.
+        :param timeout: is the time in seconds that the planner has at max to solve the problem, defaults to `None`.
+        :param output_stream: is a stream of strings where the planner writes his
+            output (and also errors) while it is solving the problem; defaults to `None`.
+        :return: an iterator of `PlanGenerationResult` created by the planner.
+
+        The only required parameter is `problem` but the planner should warn the user if `timeout` or
+        `output_stream` are not `None` and the planner ignores them."""
+        assert isinstance(self, up.engines.engine.Engine)
+        if not self.supports(problem.kind):
+            raise up.exceptions.UPUsageError(
+                f"{self.name} cannot solve this kind of problem!"
+            )
+        for res in self._get_solutions(problem, timeout, output_stream):
+            yield res
+
+    def _get_solutions(
+        self,
+        problem: "up.model.AbstractProblem",
+        timeout: Optional[float] = None,
+        output_stream: Optional[IO[str]] = None,
+    ) -> Iterator["up.engines.results.PlanGenerationResult"]:
+        """
+        Method called by the AnytimePlannerMixin.get_solutions method that has to be implemented
         by the engines that implement this operation mode.
         """
         raise NotImplementedError
