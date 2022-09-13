4f8a1c48ffecb8465654d47b94abbb7a57166a4a
Luca Framba <framba@fbk.eu>
2022-09-13 12:09:07 +0200

    Starting implementation of an ANML Parser

---
+++ test_anml_parsing.py
@@ -0,0 +1,13 @@
+from unified_planning.io import ANMLReader
+
+pr = ANMLReader()
+problem = pr.parse_problem("./anml_files/match.anml")
+# print(problem)
+
+"""
+:class:`~unified_planning.model.Problem`
+`~
+``
+~
+:func:`timed_effects <unified_planning.model.Problem.timed_effects>`
+ """
