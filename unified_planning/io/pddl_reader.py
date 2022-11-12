afaafe20c82a80fa84c7730fa39db63c45a3099b
stoppini <rove.albe@gmail.com>
2022-11-12 19:10:34 +0100

    Fix test

--- unified_planning/io/pddl_reader.py
+++ unified_planning/io/pddl_reader.py
@@ -78,7 +78,7 @@ class PDDLGrammar:
             + ":requirements"
             + OneOrMore(
                 one_of(
-                    ":strips :typing :negative-preconditions :disjunctive-preconditions :equality :existential-preconditions :universal-preconditions :quantified-preconditions :conditional-effects :fluents :numeric-fluents :adl :durative-actions :duration-inequalities :timed-initial-literals :action-costs :hierarchy :method-preconditions, :constraints"
+                    ":strips :typing :negative-preconditions :disjunctive-preconditions :equality :existential-preconditions :universal-preconditions :quantified-preconditions :conditional-effects :fluents :numeric-fluents :adl :durative-actions :duration-inequalities :timed-initial-literals :action-costs :hierarchy :method-preconditions :constraints"
                 )
             )
             + Suppress(")")
