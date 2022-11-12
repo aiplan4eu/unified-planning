221603bca5e801887bf7c90f15867c486e7ae505
alee08 <ale.trapasso8@gmail.com>
2022-11-12 18:01:02 +0100

    Fix

--- unified_planning/io/__init__.py
+++ unified_planning/io/__init__.py
@@ -2,3 +2,4 @@ from unified_planning.io.python_writer import PythonWriter
 from unified_planning.io.pddl_reader import PDDLReader
 from unified_planning.io.pddl_writer import PDDLWriter
 from unified_planning.io.anml_writer import ANMLWriter
+from unified_planning.io.ma_pddl_writer import MAPDDLWriter
