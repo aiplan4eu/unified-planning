b706c8ccc199e84ebad2d98e15919c719fd50316
alee08 <ale.trapasso8@gmail.com>
2023-01-18 16:57:24 +0100

    Updates

--- unified_planning/io/pddl_writer.py
+++ unified_planning/io/pddl_writer.py
@@ -871,8 +871,6 @@ def _get_pddl_name(
         "up.model.Parameter",
         "up.model.Variable",
         "up.model.Problem",
-        "up.model.multi_agent.MultiAgentProblem",
-        "up.model.multi_agent.Agent",
     ]
 ) -> str:
     """This function returns a pddl name for the chosen item"""
