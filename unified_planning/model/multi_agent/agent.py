8bce2deb9fd7cb2f2612af24163d381172f33395
Luca Framba <framba@fbk.eu>
2023-02-08 11:52:37 +0100

    Removed ma_problem field from agent and removed fmap from default engines

--- unified_planning/model/multi_agent/agent.py
+++ unified_planning/model/multi_agent/agent.py
@@ -44,7 +44,6 @@ class Agent(
         ActionsSetMixin.__init__(
             self, ma_problem.env, ma_problem._add_user_type, self.has_name
         )
-        self._ma_problem = ma_problem
         self._env = ma_problem.env
         self._name: str = name
 
