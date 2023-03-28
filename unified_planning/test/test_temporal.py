5dfd8d8fe39c79f2cd37e78bbd33407edf12e1fe
Luca Framba <framba@fbk.eu>
2023-03-28 10:58:15 +0200

    Added a test on increasing epsilon in a problem and updated tamer commit

--- unified_planning/test/test_temporal.py
+++ unified_planning/test/test_temporal.py
@@ -14,7 +14,12 @@
 
 import unified_planning
 from unified_planning.shortcuts import *
-from unified_planning.model.problem_kind import basic_temporal_kind, temporal_kind
+from unified_planning.model.problem_kind import (
+    basic_temporal_kind,
+    temporal_kind,
+    classical_kind,
+)
+from unified_planning.plans import TimeTriggeredPlan
 from unified_planning.test import TestCase, main, skipIfNoOneshotPlannerForProblemKind
 from unified_planning.test.examples import get_example_problems
 
@@ -24,7 +29,7 @@ class TestTemporalPlanner(TestCase):
         TestCase.setUp(self)
         self.problems = get_example_problems()
 
-    @skipIfNoOneshotPlannerForProblemKind(basic_temporal_kind)
+    @skipIfNoOneshotPlannerForProblemKind(basic_temporal_kind.union(classical_kind))
     def test_matchcellar(self):
         problem = self.problems["matchcellar"].problem
 
@@ -41,3 +46,32 @@ class TestTemporalPlanner(TestCase):
             self.assertNotEqual(planner, None)
             plan = planner.solve(problem).plan
             self.assertEqual(len(plan.timed_actions), 4)
+
+    @skipIfNoOneshotPlannerForProblemKind(basic_temporal_kind.union(classical_kind))
+    def test_epsilon(self):
+        # Test the matchcellar problem with different increasing required epsilons,
+        # assuming that the planner will never return a plan after the epsilon becomes
+        # too big to handle
+
+        # List that can be modified with the Epsilons to test.
+        epsilons_str = ["10", "1", "0.1", "0.01", "0.001", "0.0001"]
+
+        problem = self.problems["matchcellar"].problem
+        problem = problem.clone()
+
+        epsilons = tuple(map(Fraction, epsilons_str))
+        epsilon_too_high = False
+        with OneshotPlanner(problem_kind=problem.kind) as planner:
+            self.assertNotEqual(planner, None)
+            for eps in sorted(epsilons):
+                problem.epsilon = eps
+                plan = planner.solve(problem).plan
+                if plan is not None:
+                    self.assertFalse(epsilon_too_high)
+                    assert isinstance(plan, TimeTriggeredPlan)
+                    problem_epsilon = problem.epsilon
+                    plan_epsilon = plan.extract_epsilon(problem)
+                    assert plan_epsilon is not None
+                    self.assertGreaterEqual(plan_epsilon, problem_epsilon)
+                else:
+                    epsilon_too_high = True
