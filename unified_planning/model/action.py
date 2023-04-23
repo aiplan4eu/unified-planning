b5cec7873fc52b8e1554de194c9685a0229f5520
alee08 <ale.trapasso8@gmail.com>
2023-04-23 20:16:27 +0200

    Updated UPUsageError in add_action

--- unified_planning/model/action.py
+++ unified_planning/model/action.py
@@ -323,7 +323,7 @@ class InstantaneousAction(Action):
         ) = self._environment.expression_manager.auto_promote(fluent, value, condition)
         if not fluent_exp.is_fluent_exp() and not fluent_exp.is_dot():
             raise UPUsageError(
-                "fluent field of add_effect must be a Fluent or a FluentExp"
+                "fluent field of add_effect must be a Fluent or a FluentExp or a Dot."
             )
         if not self._environment.type_checker.get_type(condition_exp).is_bool_type():
             raise UPTypeError("Effect condition is not a Boolean condition!")
@@ -355,9 +355,9 @@ class InstantaneousAction(Action):
             value_exp,
             condition_exp,
         ) = self._environment.expression_manager.auto_promote(fluent, value, condition)
-        if not fluent_exp.is_fluent_exp():
+        if not fluent_exp.is_fluent_exp() and not fluent_exp.is_dot():
             raise UPUsageError(
-                "fluent field of add_increase_effect must be a Fluent or a FluentExp"
+                "fluent field of add_increase_effect must be a Fluent or a FluentExp or a Dot."
             )
         if not condition_exp.type.is_bool_type():
             raise UPTypeError("Effect condition is not a Boolean condition!")
@@ -395,9 +395,9 @@ class InstantaneousAction(Action):
             value_exp,
             condition_exp,
         ) = self._environment.expression_manager.auto_promote(fluent, value, condition)
-        if not fluent_exp.is_fluent_exp():
+        if not fluent_exp.is_fluent_exp() and not fluent_exp.is_dot():
             raise UPUsageError(
-                "fluent field of add_decrease_effect must be a Fluent or a FluentExp"
+                "fluent field of add_decrease_effect must be a Fluent or a FluentExp or a Dot."
             )
         if not condition_exp.type.is_bool_type():
             raise UPTypeError("Effect condition is not a Boolean condition!")
