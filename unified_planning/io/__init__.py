a92b71f7566c762c4ea83347543cd6ce36478b3e
Luca Framba <framba@fbk.eu>
2023-05-08 11:07:11 +0200

    removed python writer

--- unified_planning/io/__init__.py
+++ unified_planning/io/__init__.py
@@ -1,4 +1,3 @@
-from unified_planning.io.python_writer import PythonWriter
 from unified_planning.io.pddl_reader import PDDLReader
 from unified_planning.io.pddl_writer import PDDLWriter
 from unified_planning.io.anml_writer import ANMLWriter
