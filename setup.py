2dc8eae49eba59fcb9839fc54f55ee437a9e5c67
Alessandro Valentini <alvalentini@fbk.eu>
2023-05-08 14:52:24 +0200

    Updated up-fmap version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "enhsp": ["up-enhsp==0.0.13"],
         "fast-downward": ["up-fast-downward==0.2.1"],
         "lpg": ["up-lpg==0.0.6.3"],
-        "fmap": ["up-fmap==0.0.6"],
+        "fmap": ["up-fmap==0.0.7"],
         "aries": ["up-aries>=0.0.8"],
         "engines": [
             "tarski[arithmetic]",
@@ -39,7 +39,7 @@ setup(
             "up-enhsp==0.0.13",
             "up-fast-downward==0.2.1",
             "up-lpg==0.0.6.3",
-            "up-fmap==0.0.6",
+            "up-fmap==0.0.7",
             "up-aries>=0.0.8",
         ],
     },
