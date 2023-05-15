46a1d48f62727b51ffb509ada239ebcae8884d60
alee08 <ale.trapasso8@gmail.com>
2023-05-15 15:14:00 +0200

    Minor fix

--- unified_planning/engines/compilers/ma_conditional_effects_remover.py
+++ unified_planning/engines/compilers/ma_conditional_effects_remover.py
@@ -130,7 +130,7 @@ class MAConditionalEffectsRemover(ConditionalEffectsRemover):
     ) -> ProblemKind:
         new_kind = ProblemKind(problem_kind.features)
         if new_kind.has_conditional_effects():
-            new_kind.unset_effects_kind("MA_CONDITIONAL_EFFECTS")
+            new_kind.unset_effects_kind("CONDITIONAL_EFFECTS")
             new_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
         return new_kind
 
