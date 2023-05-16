4c1012e9692a85cf943c01698e70b34ec9a348cb
Luca Framba <framba@fbk.eu>
2023-05-16 11:57:06 +0200

    Added some refactoring of code between the single-agent version of compilers and the multi-agent one

--- unified_planning/engines/compilers/ma_conditional_effects_remover.py
+++ unified_planning/engines/compilers/ma_conditional_effects_remover.py
@@ -12,46 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""This module defines the dnf remover class."""
+"""This module defines the multi-agent conditional effects remover class."""
 
 import unified_planning as up
 import unified_planning.engines as engines
 from unified_planning.engines.mixins.compiler import CompilationKind, CompilerMixin
-from unified_planning.engines.compilers.utils import get_fresh_name, replace_action
+from unified_planning.engines.compilers.utils import replace_action
 from unified_planning.engines.results import CompilerResult
-from unified_planning.exceptions import (
-    UPProblemDefinitionError,
-    UPConflictingEffectsException,
-)
 from unified_planning.model import (
-    FNode,
-    Problem,
-    InstantaneousAction,
-    DurativeAction,
-    TimeInterval,
-    Timing,
-    Action,
     ProblemKind,
-    Oversubscription,
 )
-from unified_planning.model.walkers import Dnf
-from itertools import product
 from functools import partial
 from unified_planning.engines.compilers.conditional_effects_remover import (
     ConditionalEffectsRemover,
 )
 from unified_planning.model.multi_agent.ma_problem import MultiAgentProblem
-from unified_planning.model.multi_agent.agent import Agent
-from typing import Dict, Iterable, List, Optional, Tuple, cast
 
-from unified_planning.model import Problem, ProblemKind, MinimizeActionCosts
+from unified_planning.model import ProblemKind
 from unified_planning.engines.compilers.utils import (
-    get_fresh_name,
-    check_and_simplify_preconditions,
-    check_and_simplify_conditions,
     replace_action,
 )
-from unified_planning.utils import powerset
 
 
 class MAConditionalEffectsRemover(ConditionalEffectsRemover):
@@ -81,58 +61,14 @@ class MAConditionalEffectsRemover(ConditionalEffectsRemover):
 
     @staticmethod
     def supported_kind() -> ProblemKind:
-        supported_kind = ProblemKind()
+        supported_kind = ConditionalEffectsRemover.supported_kind()
+        supported_kind.unset_problem_class("ACTION_BASED")
         supported_kind.set_problem_class("ACTION_BASED_MULTI_AGENT")
-        supported_kind.set_typing("FLAT_TYPING")
-        supported_kind.set_typing("HIERARCHICAL_TYPING")
-        supported_kind.set_numbers("CONTINUOUS_NUMBERS")
-        supported_kind.set_numbers("DISCRETE_NUMBERS")
-        supported_kind.set_numbers("BOUNDED_TYPES")
-        supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
-        supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
-        supported_kind.set_fluents_type("NUMERIC_FLUENTS")
-        supported_kind.set_fluents_type("OBJECT_FLUENTS")
-        supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
-        supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
-        supported_kind.set_conditions_kind("EQUALITIES")
-        supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
-        supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
-        supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
-        supported_kind.set_effects_kind("INCREASE_EFFECTS")
-        supported_kind.set_effects_kind("DECREASE_EFFECTS")
-        supported_kind.set_time("CONTINUOUS_TIME")
-        supported_kind.set_time("DISCRETE_TIME")
-        supported_kind.set_time("INTERMEDIATE_CONDITIONS_AND_EFFECTS")
-        supported_kind.set_time("EXTERNAL_CONDITIONS_AND_EFFECTS")
-        supported_kind.set_time("TIMED_EFFECTS")
-        supported_kind.set_time("TIMED_GOALS")
-        supported_kind.set_time("DURATION_INEQUALITIES")
-        supported_kind.set_simulated_entities("SIMULATED_EFFECTS")
-        supported_kind.set_quality_metrics("ACTIONS_COST")
-        supported_kind.set_quality_metrics("PLAN_LENGTH")
-        supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
-        supported_kind.set_quality_metrics("TEMPORAL_OVERSUBSCRIPTION")
-        supported_kind.set_quality_metrics("MAKESPAN")
-        supported_kind.set_quality_metrics("FINAL_VALUE")
         return supported_kind
 
     @staticmethod
     def supports(problem_kind):
         return problem_kind <= MAConditionalEffectsRemover.supported_kind()
-
-    @staticmethod
-    def supports_compilation(compilation_kind: CompilationKind) -> bool:
-        return compilation_kind == CompilationKind.CONDITIONAL_EFFECTS_REMOVING
-
-    @staticmethod
-    def resulting_problem_kind(
-        problem_kind: ProblemKind, compilation_kind: Optional[CompilationKind] = None
-    ) -> ProblemKind:
-        new_kind = ProblemKind(problem_kind.features)
-        if new_kind.has_conditional_effects():
-            new_kind.unset_effects_kind("CONDITIONAL_EFFECTS")
-            new_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
-        return new_kind
 
     def _compile(
         self,
@@ -151,8 +87,6 @@ class MAConditionalEffectsRemover(ConditionalEffectsRemover):
             :class:`~unified_planning.model.Effect` can't be removed without changing the :class:`~unified_planning.model.Problem` semantic.
         """
         assert isinstance(problem, MultiAgentProblem)
-        env = problem.environment
-        simplifier = env.simplifier
 
         new_to_old = {}
 
@@ -167,96 +101,11 @@ class MAConditionalEffectsRemover(ConditionalEffectsRemover):
                 new_ag.add_action(new_uncond_action)
                 new_to_old[new_uncond_action] = ua
             for action in ag.conditional_actions:
-                if isinstance(action, up.model.InstantaneousAction):
-                    cond_effects = action.conditional_effects
-                    for p in powerset(range(len(cond_effects))):
-                        new_action = action.clone()
-                        new_action.name = get_fresh_name(new_problem, action.name)
-                        new_action.clear_effects()
-                        for e in action.unconditional_effects:
-                            new_action._add_effect_instance(e.clone())
-                        for i, e in enumerate(cond_effects):
-                            if i in p:
-                                # positive precondition
-                                new_action.add_precondition(e.condition)
-                                ne = up.model.Effect(
-                                    e.fluent,
-                                    e.value,
-                                    env.expression_manager.TRUE(),
-                                    e.kind,
-                                )
-                                # We try to add the new effect, but it might be in conflict with exising effects,
-                                # so the action is not added to the problem
-                                try:
-                                    new_action._add_effect_instance(ne)
-                                except UPConflictingEffectsException:
-                                    continue
-                            else:
-                                # negative precondition
-                                new_action.add_precondition(
-                                    env.expression_manager.Not(e.condition)
-                                )
-                        # new action is created, then is checked if it has any impact and if it can be simplified
-                        if len(new_action.effects) > 0:
-                            (
-                                action_is_feasible,
-                                simplified_preconditions,
-                            ) = check_and_simplify_preconditions(
-                                new_problem, new_action, simplifier
-                            )
-                            if action_is_feasible:
-                                new_action._set_preconditions(simplified_preconditions)
-                                new_to_old[new_action] = action
-                                new_ag.add_action(new_action)
-                elif isinstance(action, up.model.DurativeAction):
-                    timing_cond_effects: Dict[
-                        "up.model.Timing", List["up.model.Effect"]
-                    ] = action.conditional_effects
-                    cond_effects_timing: List[
-                        Tuple["up.model.Effect", "up.model.Timing"]
-                    ] = [(e, t) for t, el in timing_cond_effects.items() for e in el]
-                    for p in powerset(range(len(cond_effects_timing))):
-                        new_action = action.clone()
-                        new_action.name = get_fresh_name(new_problem, action.name)
-                        new_action.clear_effects()
-                        for t, el in action.unconditional_effects.items():
-                            for e in el:
-                                new_action._add_effect_instance(t, e.clone())
-                        for i, (e, t) in enumerate(cond_effects_timing):
-                            if i in p:
-                                # positive precondition
-                                new_action.add_condition(t, e.condition)
-                                ne = up.model.Effect(
-                                    e.fluent,
-                                    e.value,
-                                    env.expression_manager.TRUE(),
-                                    e.kind,
-                                )
-                                # We try to add the new effect, but it might be in conflict with exising effects,
-                                # so the action is not added to the problem
-                                try:
-                                    new_action._add_effect_instance(t, ne)
-                                except UPConflictingEffectsException:
-                                    continue
-                            else:
-                                # negative precondition
-                                new_action.add_condition(
-                                    t, env.expression_manager.Not(e.condition)
-                                )
-                        # new action is created, then is checked if it has any impact and if it can be simplified
-                        if len(new_action.effects) > 0:
-                            (
-                                action_is_feasible,
-                                simplified_conditions,
-                            ) = check_and_simplify_conditions(
-                                new_problem, new_action, simplifier
-                            )
-                            if action_is_feasible:
-                                new_action.clear_conditions()
-                                for interval, c in simplified_conditions:
-                                    new_action.add_condition(interval, c)
-                                new_to_old[new_action] = action
-                                new_ag.add_action(new_action)
+                for new_action in self._create_unconditional_actions(
+                    action, new_problem
+                ):
+                    new_to_old[new_action] = action
+                    new_ag.add_action(new_action)
                 else:
                     raise NotImplementedError
             new_problem.add_agent(new_ag)
