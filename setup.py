214d8e7832971d6f9ad4303035e49d134d7069bf
Javier Segovia <jsego6@gmail.com>
2023-05-25 19:00:03 +0200

    Registering planner

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "lpg": ["up-lpg==0.0.6.3"],
         "fmap": ["up-fmap==0.0.6"],
         "aries": ["up-aries>=0.0.8"],
+        "bfgp": ["up-bfgp==0.0.1"]
         "engines": [
             "tarski[arithmetic]",
             "up-pyperplan==0.3.0.4.dev1",
@@ -41,6 +42,7 @@ setup(
             "up-lpg==0.0.6.3",
             "up-fmap==0.0.6",
             "up-aries>=0.0.8",
+            "up-bfgp==0.0.1"
         ],
     },
     license="APACHE",
