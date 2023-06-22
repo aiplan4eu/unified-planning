81c971ad31148b2eb34f5ad03bc9e4adf1558432
Alessandro Valentini <alvalentini@fbk.eu>
2023-06-22 11:02:02 +0200

    Fixed copyright

--- unified_planning/model/htn/task.py
+++ unified_planning/model/htn/task.py
@@ -1,4 +1,4 @@
-# Copyright 2022 AIPlan4EU project
+# Copyright 2021-2023 AIPlan4EU project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
