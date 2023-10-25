c22c047edfdcf43b1cbc8920674263cbc92e40c2
Anissa <aabansir@fbk.eu>
2023-10-25 14:14:23 +0200

    [ADD] Complete partial order plan to STN with Start and End nodes

--- test_quick.py
+++ test_quick.py
@@ -6,6 +6,8 @@ up.shortcuts.get_environment().credits_stream = None
 
 from unified_planning.plans.ttp_to_stn import *
 from unified_planning.test.examples import get_example_problems
+import pylab
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -15,6 +17,19 @@ if __name__ == "__main__":
     with OneshotPlanner(problem_kind=problem.kind) as planner:
         plan = planner.solve(problem).plan
     ttp_to_stn = TTP_to_STN(plan, problem)
-    # ttp_to_stn.get_table_event()
-    ttp_to_stn.run()
+    print("PLAN :", plan)
+    graph = ttp_to_stn.run()
+
+    options = {
+    'node_color': 'blue',
+    'node_size': 1000,
+    'width': 3,
+}
+    pos=nx.spring_layout(ttp_to_stn.stn)   
+    edge_labels=dict([((u,v,),[float(d['interval'][0]), float(d['interval'][0])])
+                 for u,v,d in ttp_to_stn.stn.edges(data=True)])
+    nx.draw_networkx_edge_labels(ttp_to_stn.stn, pos, edge_labels=edge_labels)
+    nx.draw_networkx(ttp_to_stn.stn, pos, with_labels = True, arrows=True, **options, edge_cmap=plt.cm.Reds)
+    pylab.show()
+
     print("Done...")
--- unified_planning/plans/ttp_to_stn.py
+++ unified_planning/plans/ttp_to_stn.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 import networkx as nx
+import re
+import copy
 import unified_planning as up
 from unified_planning.plans.time_triggered_plan import _absolute_time
 from unified_planning.plans.time_triggered_plan import *
@@ -7,7 +9,7 @@ from unified_planning.plans.plan import ActionInstance
 from unified_planning.model.mixins.timed_conds_effs import *
 from unified_planning.plans.sequential_plan import SequentialPlan
 from unified_planning.plans.partial_order_plan import PartialOrderPlan
-from unified_planning.model.action import Action
+from unified_planning.plot.plan_plot import plot_partial_order_plan
 
 
 def is_time_in_interv(
@@ -45,7 +47,8 @@ class TTP_to_STN:
         self.events: List[Tuple[Fraction, InstantaneousAction]] = []
         self.events_sorted: List[Tuple[Fraction, InstantaneousAction]] = []
         self.seq_plan: SequentialPlan
-        self.partial_order_plan: "up.plans.partial_order_plan.PartialOrderPlan"
+        self.partial_order_plan: PartialOrderPlan
+        self.stn: nx.DiGraph
 
     def sort_condition(
         self,
@@ -130,13 +133,10 @@ class TTP_to_STN:
 
     def sort_events(self) -> List[Tuple[Fraction, InstantaneousAction]]:
         self.table_to_events()
-        print("BEFORE")
-        for tim, act in self.events:
-            print("%s: %s" % (float(tim), act._name))
 
         self.events_sorted = sorted(self.events, key=lambda acts: acts[0])
 
-        print("\r\nAFTER")
+        print("\r\nAFTER SORT")
         for tim, act in self.events_sorted:
             print("%s: %s" % (float(tim), act._name))
         return self.events_sorted
@@ -145,17 +145,77 @@ class TTP_to_STN:
         self.sort_events()
         list_act = [ActionInstance(i[1]) for i in self.events_sorted]
         seqplan = SequentialPlan(list_act)
-        print("\r\nseq plan", seqplan)
         partial_order_plan = seqplan._to_partial_order_plan(self.problem)
         self.partial_order_plan = partial_order_plan
         print("\r\npartial order plan ", partial_order_plan)
         return partial_order_plan
 
     def partial_order_plan_to_stn(self) -> nx.DiGraph:
-        return
+        graph = self.partial_order_plan._graph
+
+        for ai_current, l_next_ai in self.partial_order_plan.get_adjacency_list.items():
+            ai_current_time = [
+                item[0] for item in self.events_sorted if item[1] == ai_current.action
+            ][0]
+            for ai_next in l_next_ai:
+                ai_next_time = [
+                    item[0] for item in self.events_sorted if item[1] == ai_next.action
+                ][0]
+                time_edge = ai_next_time - ai_current_time
+                graph[ai_current][ai_next]["interval"] = [
+                    time_edge,
+                    time_edge,
+                ]  # TODO get time with interval correctly
+
+
+        # Add start and end nodes
+        
+        start = ActionInstance(InstantaneousAction("START"))
+        end = ActionInstance(InstantaneousAction("END"))
+        graph.add_node(start)
+        graph.add_node(end)
+        graph.add_edge(start, list(self.partial_order_plan.get_adjacency_list.keys())[0], interval= [0.0,0.0])
+        graph.add_edge([item for item in list(self.partial_order_plan.get_adjacency_list.keys())[::-1] if item.action._name != "START" and item.action._name != "END"][0], end, interval= [0.0,0.0])
+
+        self.stn = graph
+        return graph
+
+    def stn_clean(self, graph: nx.DiGraph):
+        nodes_list = list(graph.nodes(data=True))
+
+        to_jump = []
+        for node, data in nodes_list:
+            if node in to_jump:
+                continue
+            neighbors_node = copy.copy(graph.neighbors(node))
+            for next_node in neighbors_node:
+                if bool(re.match("(.*)(end|start) \+ [0-9]+$", next_node.action._name)):
+                    neighbors_next_node = copy.copy(graph.neighbors(next_node))
+                    for next_next_node in neighbors_next_node:
+                        if next_next_node == node:
+                            continue
+                        graph.add_edge(node, next_next_node)
+                        a = graph[node][next_node]["interval"]
+                        b = graph[next_node][next_next_node]["interval"]
+                        graph[node][next_next_node]["interval"] = [
+                            a[0] + b[0],
+                            a[1] + b[1],
+                        ]
+                        graph.remove_edge(
+                            next_node, next_next_node
+                        )  # is it usefull ? because delete node at the end
+
+                    graph.remove_edge(
+                        node, next_node
+                    )  # is it usefull ? because delete node at the end
+                    graph.remove_node(next_node)
+                    to_jump += [item[0] for item in nodes_list if item[0] == next_node]
+        
+
 
     def run(self):
         # partial order plan to STN
         self.events_to_partial_order_plan()
-        self.partial_order_plan_to_stn()
-        return
+        graph = self.partial_order_plan_to_stn()
+        self.stn_clean(graph)
+        return graph
