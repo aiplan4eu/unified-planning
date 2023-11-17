760ee884c3164e1ebd18fb692db229680bd40713
Andrea Micheli <amicheli@fbk.eu>
2023-11-17 17:54:48 +0100

    Added trace to sequential validator and started mock-up of timetriggered plan validator

--- unified_planning/engines/plan_validator.py
+++ unified_planning/engines/plan_validator.py
@@ -14,7 +14,7 @@
 #
 
 
-from typing import Optional, cast
+from typing import List, Optional, cast
 import warnings
 import unified_planning as up
 import unified_planning.environment
@@ -48,6 +48,7 @@ from unified_planning.exceptions import (
     UPProblemDefinitionError,
     UPInvalidActionError,
 )
+from unified_planning.plans.time_triggered_plan import TimeTriggeredPlan
 
 
 class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
@@ -66,8 +67,6 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
                 options.get("environment", None)
             )
         )
-        self.manager = self._env.expression_manager
-        self._substituter = walkers.Substituter(self._env)
 
     @property
     def name(self):
@@ -79,52 +78,7 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
 
     @staticmethod
     def supported_kind() -> ProblemKind:
-        supported_kind = ProblemKind(version=LATEST_PROBLEM_KIND_VERSION)
-        supported_kind.set_problem_class("ACTION_BASED")
-        supported_kind.set_typing("FLAT_TYPING")
-        supported_kind.set_typing("HIERARCHICAL_TYPING")
-        supported_kind.set_parameters("BOOL_FLUENT_PARAMETERS")
-        supported_kind.set_parameters("BOUNDED_INT_FLUENT_PARAMETERS")
-        supported_kind.set_parameters("BOOL_ACTION_PARAMETERS")
-        supported_kind.set_parameters("BOUNDED_INT_ACTION_PARAMETERS")
-        supported_kind.set_parameters("UNBOUNDED_INT_ACTION_PARAMETERS")
-        supported_kind.set_parameters("REAL_ACTION_PARAMETERS")
-        supported_kind.set_numbers("BOUNDED_TYPES")
-        supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
-        supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
-        supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
-        supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
-        supported_kind.set_conditions_kind("EQUALITIES")
-        supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
-        supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
-        supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
-        supported_kind.set_effects_kind("INCREASE_EFFECTS")
-        supported_kind.set_effects_kind("DECREASE_EFFECTS")
-        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
-        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_NUMERIC_ASSIGNMENTS")
-        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_OBJECT_ASSIGNMENTS")
-        supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
-        supported_kind.set_effects_kind("FLUENTS_IN_NUMERIC_ASSIGNMENTS")
-        supported_kind.set_effects_kind("FLUENTS_IN_OBJECT_ASSIGNMENTS")
-        supported_kind.set_effects_kind("FORALL_EFFECTS")
-        supported_kind.set_fluents_type("INT_FLUENTS")
-        supported_kind.set_fluents_type("REAL_FLUENTS")
-        supported_kind.set_fluents_type("OBJECT_FLUENTS")
-        supported_kind.set_constraints_kind("STATE_INVARIANTS")
-        supported_kind.set_quality_metrics("ACTIONS_COST")
-        supported_kind.set_simulated_entities("SIMULATED_EFFECTS")
-        supported_kind.set_actions_cost_kind("STATIC_FLUENTS_IN_ACTIONS_COST")
-        supported_kind.set_actions_cost_kind("FLUENTS_IN_ACTIONS_COST")
-        supported_kind.set_quality_metrics("PLAN_LENGTH")
-        supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
-        supported_kind.set_quality_metrics("TEMPORAL_OVERSUBSCRIPTION")
-        supported_kind.set_quality_metrics("MAKESPAN")
-        supported_kind.set_quality_metrics("FINAL_VALUE")
-        supported_kind.set_oversubscription_kind("INT_NUMBERS_IN_OVERSUBSCRIPTION")
-        supported_kind.set_oversubscription_kind("REAL_NUMBERS_IN_OVERSUBSCRIPTION")
-        supported_kind.set_actions_cost_kind("INT_NUMBERS_IN_ACTIONS_COST")
-        supported_kind.set_actions_cost_kind("REAL_NUMBERS_IN_ACTIONS_COST")
-        return supported_kind
+        return UPSequentialSimulator.supported_kind()
 
     @staticmethod
     def supports(problem_kind):
@@ -168,16 +122,14 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
                 raise up.exceptions.UPUsageError(msg)
             else:
                 warnings.warn(cast(str, msg))
-        prev_state: Optional[State] = simulator.get_initial_state()
-        next_state: Optional[State] = prev_state
         if metric is not None:
             metric_value = evaluate_quality_metric_in_initial_state(simulator, metric)
         msg = None
+        trace: List[State] = [simulator.get_initial_state()]
         for i, ai in zip(range(1, len(plan.actions) + 1), plan.actions):
-            assert prev_state is not None
             try:
                 unsat_conds, reason = simulator.get_unsatisfied_conditions(
-                    prev_state, ai
+                    trace[-1], ai
                 )
                 if unsat_conds:
                     assert reason == InapplicabilityReasons.VIOLATES_CONDITIONS
@@ -187,7 +139,7 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
             except UPInvalidActionError as e:
                 msg = f"{str(i)}-th action instance {str(ai)} creates an Invalid Action: {str(e)}"
             try:
-                next_state = simulator.apply_unsafe(prev_state, ai)
+                next_state = simulator.apply_unsafe(trace[-1], ai)
             except UPInvalidActionError as e:
                 msg = f"{str(i)}-th action instance {str(ai)} creates an Invalid Action: {str(e)}"
             except UPConflictingEffectsException as e:
@@ -195,12 +147,13 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
             if msg is not None:
                 logs = [LogMessage(LogLevel.INFO, msg)]
                 return ValidationResult(
-                    ValidationResultStatus.INVALID,
-                    self.name,
-                    logs,
-                    None,
-                    FailedValidationReason.INAPPLICABLE_ACTION,
-                    ai,
+                    status=ValidationResultStatus.INVALID,
+                    engine_name=self.name,
+                    log_messages=logs,
+                    metric_evaluations=None,
+                    reason=FailedValidationReason.INAPPLICABLE_ACTION,
+                    inapplicable_action=ai,
+                    trace=trace,
                 )
             assert next_state is not None
             if metric is not None:
@@ -208,21 +161,25 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
                     simulator,
                     metric,
                     metric_value,
-                    prev_state,
+                    trace[-1],
                     ai.action,
                     ai.actual_parameters,
                     next_state,
                 )
-            prev_state = next_state
-        assert next_state is not None
-        unsatisfied_goals = simulator.get_unsatisfied_goals(next_state)
+            trace.append(next_state)
+
+        unsatisfied_goals = simulator.get_unsatisfied_goals(trace[-1])
         if not unsatisfied_goals:
             metric_evalutations = None
             if metric is not None:
                 metric_evalutations = {metric: metric_value}
             logs = []
             return ValidationResult(
-                ValidationResultStatus.VALID, self.name, logs, metric_evalutations
+                ValidationResultStatus.VALID,
+                self.name,
+                logs,
+                metric_evalutations,
+                trace=trace,
             )
         else:
             msg = f"Goals {unsatisfied_goals} are not satisfied by the plan."
@@ -233,4 +190,63 @@ class SequentialPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
                 logs,
                 None,
                 FailedValidationReason.UNSATISFIED_GOALS,
+                trace=trace,
             )
+
+
+class TimeTriggeredPlanValidator(engines.engine.Engine, mixins.PlanValidatorMixin):
+    """
+    Performs :class:`~unified_planning.plans.Plan` validation.
+
+    If the given :class:`~unified_planning.model.Problem` has any quality metric,
+    the metric is simply ignored because it predicates over the Optimality of
+    the Plan, but not the Validity!
+    """
+
+    def __init__(self, **options):
+        engines.engine.Engine.__init__(self)
+        self._env: "unified_planning.environment.Environment" = (
+            unified_planning.environment.get_environment(
+                options.get("environment", None)
+            )
+        )
+
+    @property
+    def name(self):
+        return "time_triggered_plan_validator"
+
+    @staticmethod
+    def supports_plan(plan_kind: "up.plans.PlanKind") -> bool:
+        return plan_kind == PlanKind.TIME_TRIGGERED_PLAN
+
+    @staticmethod
+    def supported_kind() -> ProblemKind:
+        kind = UPSequentialSimulator.supported_kind().clone()
+        kind.set_time("CONTINUOUS_TIME")
+        kind.set_time("INTERMEDIATE_CONDITIONS_AND_EFFECTS")
+        kind.set_time("TIMED_EFFECTS")
+        kind.set_time("TIMED_GOALS")
+        kind.set_time("DURATION_INEQUALITIES")
+        kind.set_expression_duration("STATIC_FLUENTS_IN_DURATIONS")
+        return kind
+
+    @staticmethod
+    def supports(problem_kind):
+        return problem_kind <= SequentialPlanValidator.supported_kind()
+
+    def _validate(
+        self, problem: "AbstractProblem", plan: "unified_planning.plans.Plan"
+    ) -> "up.engines.results.ValidationResult":
+        """
+        Returns True if and only if the plan given in input is a valid plan for the problem given in input.
+        This means that from the initial state of the problem, by following the plan, you can reach the
+        problem goal. Otherwise False is returned.
+
+        :param problem: The problem for which the plan to validate was generated.
+        :param plan: The plan that must be validated.
+        :return: The generated up.engines.results.ValidationResult; a data structure containing the information
+            about the plan validity and eventually some additional log messages for the user.
+        """
+        assert isinstance(plan, TimeTriggeredPlan)
+        assert isinstance(problem, Problem)
+        raise NotImplementedError
