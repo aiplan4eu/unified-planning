0b8c9e8bf334dd7713be08cb99b820fed060f345
Samuel Gobbi <sgobbi@fbk.eu>
2024-09-12 10:41:02 +0200

    fix(interpreted functions): various minor fixes - work in progress

--- unified_planning/model/expression.py
+++ unified_planning/model/expression.py
@@ -36,15 +36,14 @@ BoolExpression = Union[
     "up.model.parameter.Parameter",
     "up.model.variable.Variable",
     "up.model.presence.Presence",
-    "up.model.interpreted_function.InterpretedFunction",  # placeholder to fix mypy errors, not correct place, could cause errors without type checking
+    "up.model.interpreted_function.InterpretedFunction",
     bool,
 ]
 NumericConstant = Union[int, float, Fraction, str]
 NumericExpression = Union[
     NumericConstant,
     "up.model.fnode.FNode",
-    "up.model.interpreted_function.InterpretedFunction",
-]  # placeholder to fix mypy errors, not correct place, could cause errors without type checking
+]
 ConstantExpression = Union[
     NumericExpression,
     "up.model.object.Object",
