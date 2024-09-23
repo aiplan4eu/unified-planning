50e684619db16cb94221b7b44272da1acd7c41e3
Samuel Gobbi <sgobbi@fbk.eu>
2024-09-23 10:16:39 +0200

    feature(interpreted functions): added base functions for IF remover compiler

---
+++ unified_planning/engines/compilers/interpreted_functions_remover.py
@@ -0,0 +1,237 @@
+# Copyright 2021-2023 AIPlan4EU project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""This module defines the interpreted functions effects remover class."""
+
+
+import unified_planning as up
+import unified_planning.engines as engines
+from unified_planning.model.walkers.operators_extractor import OperatorsExtractor
+from unified_planning.model.operators import OperatorKind
+from unified_planning.engines.mixins.compiler import CompilationKind, CompilerMixin
+from unified_planning.engines.compilers.utils import updated_minimize_action_costs
+from unified_planning.engines.results import CompilerResult
+from unified_planning.exceptions import (
+    UPProblemDefinitionError,
+    UPConflictingEffectsException,
+)
+from unified_planning.model import (
+    Problem,
+    ProblemKind,
+    Action,
+    InstantaneousAction,
+    DurativeAction,
+    AbstractProblem,
+)
+from unified_planning.model.problem_kind_versioning import LATEST_PROBLEM_KIND_VERSION
+from unified_planning.engines.compilers.utils import (
+    get_fresh_name,
+    check_and_simplify_preconditions,
+    check_and_simplify_conditions,
+    replace_action,
+)
+from unified_planning.utils import powerset
+from typing import List, Dict, Tuple, Optional, Iterator
+from functools import partial
+
+
+class InterpretedFunctionsRemover(
+    engines.engine.Engine, CompilerMixin
+):  # change this # change this # change this
+    # change this
+    # change this
+    # change this
+    """
+    Interpreted functions remover class: this class offers the capability
+    to transform a :class:`~unified_planning.model.Problem` with interpreted functions used as conditions`
+    into a `Problem` without interpreted functions in action conditions. This capability is offered by the :meth:`~unified_planning.engines.compilers.InterpretedFunctionsRemover.compile`
+    method, that returns a :class:`~unified_planning.engines.CompilerResult` in which the :meth:`problem <unified_planning.engines.CompilerResult.problem>` field
+    is the compiled Problem.
+
+    This is done by changing every evaluation of an expression that contains an interpreted function to true.
+
+    This `Compiler` supports only the the `INTERPRETED_FUNCTIONS_REMOVER` :class:`~unified_planning.engines.CompilationKind`.
+    """
+
+    def __init__(self):
+        engines.engine.Engine.__init__(self)
+        CompilerMixin.__init__(self, CompilationKind.INTERPRETED_FUNCTIONS_REMOVING)
+
+    @property
+    def name(self):
+        return "ifrm"
+
+    @staticmethod  # change this # change this
+    def supported_kind() -> ProblemKind:  # change this # change this
+        supported_kind = ProblemKind(version=LATEST_PROBLEM_KIND_VERSION)
+        supported_kind.set_problem_class("ACTION_BASED")
+        supported_kind.set_typing("FLAT_TYPING")
+        supported_kind.set_typing("HIERARCHICAL_TYPING")
+        supported_kind.set_parameters("BOOL_FLUENT_PARAMETERS")
+        supported_kind.set_parameters("BOUNDED_INT_FLUENT_PARAMETERS")
+        supported_kind.set_parameters("BOOL_ACTION_PARAMETERS")
+        supported_kind.set_parameters("BOUNDED_INT_ACTION_PARAMETERS")
+        supported_kind.set_parameters("UNBOUNDED_INT_ACTION_PARAMETERS")
+        supported_kind.set_parameters("REAL_ACTION_PARAMETERS")
+        supported_kind.set_numbers("BOUNDED_TYPES")
+        supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
+        supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
+        supported_kind.set_fluents_type("INT_FLUENTS")
+        supported_kind.set_fluents_type("REAL_FLUENTS")
+        supported_kind.set_fluents_type("OBJECT_FLUENTS")
+        supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
+        supported_kind.set_conditions_kind(
+            "INTERPRETED_FUNCTIONS_IN_CONDITIONS"
+        )  # added this
+        supported_kind.set_conditions_kind("EQUALITIES")
+        supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
+        supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
+        supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
+        supported_kind.set_effects_kind("INCREASE_EFFECTS")
+        supported_kind.set_effects_kind("DECREASE_EFFECTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_OBJECT_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_OBJECT_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FORALL_EFFECTS")
+        supported_kind.set_time("CONTINUOUS_TIME")
+        supported_kind.set_time("DISCRETE_TIME")
+        supported_kind.set_time("INTERMEDIATE_CONDITIONS_AND_EFFECTS")
+        supported_kind.set_time("EXTERNAL_CONDITIONS_AND_EFFECTS")
+        supported_kind.set_time("TIMED_EFFECTS")
+        supported_kind.set_time("TIMED_GOALS")
+        supported_kind.set_time("DURATION_INEQUALITIES")
+        supported_kind.set_time("SELF_OVERLAPPING")
+        supported_kind.set_expression_duration("STATIC_FLUENTS_IN_DURATIONS")
+        supported_kind.set_expression_duration("FLUENTS_IN_DURATIONS")
+        supported_kind.set_expression_duration("INT_TYPE_DURATIONS")
+        supported_kind.set_expression_duration("REAL_TYPE_DURATIONS")
+        supported_kind.set_simulated_entities("SIMULATED_EFFECTS")
+        supported_kind.set_constraints_kind("STATE_INVARIANTS")
+        supported_kind.set_quality_metrics("ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("STATIC_FLUENTS_IN_ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("FLUENTS_IN_ACTIONS_COST")
+        supported_kind.set_quality_metrics("PLAN_LENGTH")
+        supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
+        supported_kind.set_quality_metrics("TEMPORAL_OVERSUBSCRIPTION")
+        supported_kind.set_quality_metrics("MAKESPAN")
+        supported_kind.set_quality_metrics("FINAL_VALUE")
+        supported_kind.set_actions_cost_kind("INT_NUMBERS_IN_ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("REAL_NUMBERS_IN_ACTIONS_COST")
+        supported_kind.set_oversubscription_kind("INT_NUMBERS_IN_OVERSUBSCRIPTION")
+        supported_kind.set_oversubscription_kind("REAL_NUMBERS_IN_OVERSUBSCRIPTION")
+        return supported_kind
+
+    @staticmethod
+    def supports(problem_kind):
+        return problem_kind <= InterpretedFunctionsRemover.supported_kind()
+
+    @staticmethod
+    def supports_compilation(compilation_kind: CompilationKind) -> bool:
+        return compilation_kind == CompilationKind.INTERPRETED_FUNCTIONS_REMOVING
+
+    @staticmethod
+    def resulting_problem_kind(
+        problem_kind: ProblemKind, compilation_kind: Optional[CompilationKind] = None
+    ) -> ProblemKind:
+        new_kind = problem_kind.clone()
+        if new_kind.has_interpreted_functions_in_conditions():
+            new_kind.unset_effects_kind(
+                "INTERPRETED_FUNCTIONS_IN_CONDITIONS"
+            )  # not sure something else might be necessary here
+            new_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
+        return new_kind
+
+    def _compile(
+        self,
+        problem: "up.model.AbstractProblem",
+        compilation_kind: "up.engines.CompilationKind",
+    ) -> CompilerResult:  # change this # change this # change this # change this
+        # change this
+        # change this
+        # change this
+        """
+        Takes an instance of a :class:`~unified_planning.model.Problem` and the wanted :class:`~unified_planning.engines.CompilationKind`
+        and returns a :class:`~unified_planning.engines.results.CompilerResult` where the :meth:`problem<unified_planning.engines.results.CompilerResult.problem>` field does not have conditional effects.
+
+        :param problem: The instance of the :class:`~unified_planning.model.Problem` that must be returned without conditional effects.
+        :param compilation_kind: The :class:`~unified_planning.engines.CompilationKind` that must be applied on the given problem;
+            only :class:`~unified_planning.engines.CompilationKind.CONDITIONAL_EFFECTS_REMOVING` is supported by this compiler
+        :return: The resulting :class:`~unified_planning.engines.results.CompilerResult` data structure.
+        :raises: :exc:`~unified_planning.exceptions.UPProblemDefinitionError` when the :meth:`condition<unified_planning.model.Effect.condition>` of an
+            :class:`~unified_planning.model.Effect` can't be removed without changing the :class:`~unified_planning.model.Problem` semantic.
+        """
+        assert isinstance(problem, Problem)
+        env = problem.environment
+        simplifier = env.simplifier
+
+        new_to_old: Dict[Action, Optional[Action]] = {}
+        # name_action_map: Dict[str, Union[InstantaneousAction, DurativeAction]] = {}
+        operators_extractor: up.model.walkers.OperatorsExtractor = (
+            up.model.walkers.OperatorsExtractor()
+        )
+
+        new_problem = problem.clone()
+        new_problem.name = f"{self.name}_{problem.name}"
+        new_problem.clear_actions()
+
+        for a in problem.actions:
+            if isinstance(a, InstantaneousAction):
+                no_IF_action = a.clone()
+                no_IF_action.name = get_fresh_name(new_problem, a.name)
+                no_IF_action.clear_preconditions()
+                for p in a.preconditions:
+                    # simplified_precondition = simplifier.simplify(p)
+                    # precondition_operators = operators_extractor.get(simplified_precondition)
+                    precondition_operators = operators_extractor.get(p)
+                    if not (
+                        OperatorKind.INTERPRETED_FUNCTION_EXP in precondition_operators
+                    ):
+                        no_IF_action.add_precondition(p)
+                    new_to_old[no_IF_action] = a
+            elif isinstance(a, DurativeAction):
+                no_IF_action = a.clone()
+                no_IF_action.name = get_fresh_name(new_problem, a.name)
+                no_IF_action.clear_conditions()
+                for i, cl in a.conditions.items():
+                    for c in cl:
+                        # simplified_precondition = simplifier.simplify(p)
+                        # precondition_operators = operators_extractor.get(simplified_precondition)
+                        precondition_operators = operators_extractor.get(p)
+                        if not (
+                            OperatorKind.INTERPRETED_FUNCTION_EXP
+                            in precondition_operators
+                        ):
+                            no_IF_action.add_condition(i, p)
+                    new_to_old[no_IF_action] = a
+            else:
+                raise NotImplementedError
+
+            new_problem.add_action(no_IF_action)
+            # no_IF_action = a.clone()
+            # no_IF_action.clear_preconditions()
+            # for p in a.preconditions.items():
+            #    #simplified_precondition = simplifier.simplify(p)
+            #    #precondition_operators = operators_extractor.get(simplified_precondition)
+            #    precondition_operators = operators_extractor.get(p)
+            #    if not(OperatorKind.INTERPRETED_FUNCTION_EXP in precondition_operators):
+            #        no_IF_action.add_precondition(p)
+            # new_problem.add_action(no_IF_action)
+
+        return CompilerResult(
+            new_problem, partial(replace_action, map=new_to_old), self.name
+        )
