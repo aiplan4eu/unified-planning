debe4f6feae62978d64ea1b776bbc900f1a57a5d
Samuel Gobbi <sgobbi@fbk.eu>
2024-11-05 10:24:50 +0100

    fix(interpreted functions): if values correctly passed in sequential problems

--- unified_planning/model/walkers/state_evaluator.py
+++ unified_planning/model/walkers/state_evaluator.py
@@ -28,7 +28,6 @@ class StateEvaluator(QuantifierSimplifier):
 
     def __init__(self, problem: "up.model.problem.Problem"):
         QuantifierSimplifier.__init__(self, problem.environment, problem)
-        self.if_values = {}
 
     def evaluate(
         self,
@@ -55,6 +54,8 @@ class StateEvaluator(QuantifierSimplifier):
         r = self.walk(expression)
         self._variable_assignments = None
         assert r.is_constant()
+        # print ("state evaluator - evaluate")
+        # print(self.if_values)
         return r
 
     def _deep_subs_simplify(
@@ -71,9 +72,11 @@ class StateEvaluator(QuantifierSimplifier):
         copy = self._variable_assignments.copy()
         copy.update(variables_assignments)
         r = new_state_evaluator.evaluate(expression, self._state, copy)
-        for k, v in new_state_evaluator.memoization.items():
-            if k.is_interpreted_function_exp():
-                self.if_values[k] = v
+        ## this never happens in our scenario
+        # for k, v in new_state_evaluator.memoization.items():
+        #    if k.is_interpreted_function_exp():
+        #        self.if_values[k] = v
+        #        print (self.if_values)
         assert r.is_constant()
         return r
 
