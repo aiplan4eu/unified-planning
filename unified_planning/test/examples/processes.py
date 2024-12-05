aab6be11e49a3d0d3b6d58857351287bd0d1910c
Samuel Gobbi <sgobbi@fbk.eu>
2024-12-05 11:02:20 +0100

    new test case in processes examples

--- unified_planning/test/examples/processes.py
+++ unified_planning/test/examples/processes.py
@@ -17,14 +17,8 @@ def get_example_problems():
     evt.add_precondition(GE(d, 200))
     evt.add_effect(on, False)
 
-    evt.clear_effects()
-    evt.add_effect(on, False)
-
     b = Process("moving")
     b.add_precondition(on)
-    b.add_derivative(d, 1)
-
-    b.clear_effects()
     b.add_derivative(d, 1)
 
     problem = Problem("1d_Movement")
@@ -47,4 +41,56 @@ def get_example_problems():
         invalid_plans=[],
     )
     problems["1d_movement"] = test_problem
+
+    problem = Problem("boiling_water")
+    boiler_on = Fluent("boiler_on")
+    temperature = Fluent("temperature", RealType())
+    water_level = Fluent("water_level", RealType())
+    chimney_vent_open = Fluent("chimney_vent_open")
+
+    turn_on_boiler = InstantaneousAction("turn_on_boiler")
+    turn_on_boiler.add_precondition(Not(boiler_on))
+    turn_on_boiler.add_effect(boiler_on, True)
+
+    water_heating = Process("water_heating")
+    water_heating.add_precondition(And(boiler_on, LE(temperature, 100)))
+    water_heating.add_derivative(temperature, 1)
+
+    water_boiling = Process("water_boiling")
+    water_boiling.add_precondition(And(boiler_on, GE(temperature, 100)))
+    water_boiling.add_derivative(water_level, -1)
+
+    open_chimney_vent_auto = Event("open_chimney_vent_auto")
+    open_chimney_vent_auto.add_precondition(
+        And(Not(chimney_vent_open), GE(temperature, 100))
+    )
+    open_chimney_vent_auto.add_effect(chimney_vent_open, True)
+
+    turn_off_boiler_auto = Event("turn_off_boiler_auto")
+    turn_off_boiler_auto.add_precondition(And(LE(water_level, 0), boiler_on))
+    turn_off_boiler_auto.add_effect(boiler_on, False)
+
+    problem.add_fluent(boiler_on)
+    problem.set_initial_value(boiler_on, False)
+    problem.add_fluent(chimney_vent_open)
+    problem.set_initial_value(chimney_vent_open, False)
+    problem.add_fluent(temperature)
+    problem.set_initial_value(temperature, 20)
+    problem.add_fluent(water_level)
+    problem.set_initial_value(water_level, 10)
+    problem.add_action(turn_on_boiler)
+    problem.add_process(water_heating)
+    problem.add_process(water_boiling)
+    problem.add_event(open_chimney_vent_auto)
+    problem.add_event(turn_off_boiler_auto)
+    problem.add_goal(And(Not(boiler_on), And(chimney_vent_open, LE(water_level, 2))))
+
+    test_problem = TestCase(
+        problem=problem,
+        solvable=True,
+        valid_plans=[],
+        invalid_plans=[],
+    )
+    problems["boiling_water"] = test_problem
+
     return problems
