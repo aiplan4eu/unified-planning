6b7e30561bc56a799835ca1029d2f3b965cc3134
Luca Framba <framba@fbk.eu>
2025-07-15 14:40:58 +0200

    fix(enshp_internal_engine): fix problem_kind of internal enhsp engine and updated up_enhsp_commit

--- unified_planning/test/pddl/enhsp.py
+++ unified_planning/test/pddl/enhsp.py
@@ -117,7 +117,6 @@ class ENHSP(PDDLAnytimePlanner):
     def supported_kind() -> ProblemKind:
         supported_kind = ProblemKind(version=LATEST_PROBLEM_KIND_VERSION)
         supported_kind.set_problem_class("ACTION_BASED")
-        supported_kind.set_numbers("BOUNDED_TYPES")
         supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
         supported_kind.set_typing("FLAT_TYPING")
         supported_kind.set_typing("HIERARCHICAL_TYPING")
@@ -133,7 +132,8 @@ class ENHSP(PDDLAnytimePlanner):
         supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
         supported_kind.set_effects_kind("STATIC_FLUENTS_IN_NUMERIC_ASSIGNMENTS")
         supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
-        supported_kind.set_effects_kind("FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("INCREASE_CONTINUOUS_EFFECTS")
+        supported_kind.set_effects_kind("DECREASE_CONTINUOUS_EFFECTS")
         supported_kind.set_fluents_type("INT_FLUENTS")
         supported_kind.set_fluents_type("REAL_FLUENTS")
         supported_kind.set_quality_metrics("ACTIONS_COST")
@@ -142,8 +142,9 @@ class ENHSP(PDDLAnytimePlanner):
         supported_kind.set_actions_cost_kind("INT_NUMBERS_IN_ACTIONS_COST")
         supported_kind.set_actions_cost_kind("REAL_NUMBERS_IN_ACTIONS_COST")
         supported_kind.set_actions_cost_kind("STATIC_FLUENTS_IN_ACTIONS_COST")
-        supported_kind.set_actions_cost_kind("FLUENTS_IN_ACTIONS_COST")
         supported_kind.set_time("PROCESSES")
+        supported_kind.set_time("EVENTS")
+        supported_kind.set_initial_state("UNDEFINED_INITIAL_NUMERIC")
         return supported_kind
 
     @staticmethod
