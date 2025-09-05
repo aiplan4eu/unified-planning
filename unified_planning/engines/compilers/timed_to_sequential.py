515b4bd9ad62168f5a6ba27dd151b3aa141c6596
Samuel Gobbi <sgobbi@fbk.eu>
2025-09-05 11:20:01 +0200

    feat(t2s): simple initial implementation

--- unified_planning/engines/compilers/timed_to_sequential.py
+++ unified_planning/engines/compilers/timed_to_sequential.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-"""This module defines the conditional effects remover class."""
+"""This module defines the timed to sequential problem converter class."""
 
-
-from itertools import product
 import unified_planning as up
 import unified_planning.engines as engines
 from unified_planning.engines.mixins.compiler import CompilationKind, CompilerMixin
@@ -24,32 +22,14 @@ from unified_planning.model import (
     Problem,
     ProblemKind,
     Fluent,
-    Parameter,
-    BoolExpression,
-    NumericConstant,
-    Action,
     InstantaneousAction,
     DurativeAction,
-    Effect,
-    SimulatedEffect,
-    FNode,
-    ExpressionManager,
-    MinimizeActionCosts,
-    MinimizeExpressionOnFinalState,
-    MaximizeExpressionOnFinalState,
-    Oversubscription,
-    TemporalOversubscription,
-    Object,
-    Expression,
-    DurationInterval,
-    UPState,
+    Action,
 )
+from unified_planning.model.timing import StartTiming, EndTiming
 from unified_planning.model.problem_kind_versioning import LATEST_PROBLEM_KIND_VERSION
-from unified_planning.model.walkers import UsertypeFluentsWalker
-from unified_planning.model.types import _UserType
 from unified_planning.engines.compilers.utils import replace_action
-from unified_planning.model.fluent import get_all_fluent_exp
-from typing import Iterator, Dict, List, OrderedDict, Set, Tuple, Optional, Union, cast
+from typing import Dict, Optional, List
 from functools import partial
 
 
@@ -69,64 +49,65 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
     @staticmethod
     def supported_kind() -> ProblemKind:
         supported_kind = ProblemKind(version=LATEST_PROBLEM_KIND_VERSION)
-        # supported_kind.set_problem_class("ACTION_BASED")
-        # supported_kind.set_typing("FLAT_TYPING")
-        # supported_kind.set_typing("HIERARCHICAL_TYPING")
-        # supported_kind.set_parameters("BOOL_FLUENT_PARAMETERS")
-        # supported_kind.set_parameters("BOUNDED_INT_FLUENT_PARAMETERS")
-        # supported_kind.set_parameters("BOOL_ACTION_PARAMETERS")
-        # supported_kind.set_parameters("BOUNDED_INT_ACTION_PARAMETERS")
-        # supported_kind.set_parameters("UNBOUNDED_INT_ACTION_PARAMETERS")
-        # supported_kind.set_parameters("REAL_ACTION_PARAMETERS")
-        # supported_kind.set_numbers("BOUNDED_TYPES")
-        # supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
-        # supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
-        # supported_kind.set_fluents_type("INT_FLUENTS")
-        # supported_kind.set_fluents_type("REAL_FLUENTS")
-        # supported_kind.set_fluents_type("OBJECT_FLUENTS")
-        # supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
-        # supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
-        # supported_kind.set_conditions_kind("EQUALITIES")
-        # supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
-        # supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
-        # supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
-        # supported_kind.set_effects_kind("INCREASE_EFFECTS")
-        # supported_kind.set_effects_kind("DECREASE_EFFECTS")
-        # supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
-        # supported_kind.set_effects_kind("STATIC_FLUENTS_IN_NUMERIC_ASSIGNMENTS")
-        # supported_kind.set_effects_kind("STATIC_FLUENTS_IN_OBJECT_ASSIGNMENTS")
-        # supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
-        # supported_kind.set_effects_kind("FLUENTS_IN_NUMERIC_ASSIGNMENTS")
-        # supported_kind.set_effects_kind("FLUENTS_IN_OBJECT_ASSIGNMENTS")
-        # supported_kind.set_effects_kind("FORALL_EFFECTS")
-        # supported_kind.set_time("CONTINUOUS_TIME")
-        # supported_kind.set_time("DISCRETE_TIME")
+        # TODO timed? conditional effects? are supported or not?
+        supported_kind.set_problem_class("ACTION_BASED")
+        supported_kind.set_typing("FLAT_TYPING")
+        supported_kind.set_typing("HIERARCHICAL_TYPING")
+        supported_kind.set_parameters("BOOL_FLUENT_PARAMETERS")
+        supported_kind.set_parameters("BOUNDED_INT_FLUENT_PARAMETERS")
+        supported_kind.set_parameters("BOOL_ACTION_PARAMETERS")
+        supported_kind.set_parameters("BOUNDED_INT_ACTION_PARAMETERS")
+        supported_kind.set_parameters("UNBOUNDED_INT_ACTION_PARAMETERS")
+        supported_kind.set_parameters("REAL_ACTION_PARAMETERS")
+        supported_kind.set_numbers("BOUNDED_TYPES")
+        supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
+        supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
+        supported_kind.set_fluents_type("INT_FLUENTS")
+        supported_kind.set_fluents_type("REAL_FLUENTS")
+        supported_kind.set_fluents_type("OBJECT_FLUENTS")
+        supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("EQUALITIES")
+        supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
+        supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
+        supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
+        supported_kind.set_effects_kind("INCREASE_EFFECTS")
+        supported_kind.set_effects_kind("DECREASE_EFFECTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_OBJECT_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_OBJECT_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FORALL_EFFECTS")
+        supported_kind.set_time("CONTINUOUS_TIME")
+        supported_kind.set_time("DISCRETE_TIME")
         # supported_kind.set_time("INTERMEDIATE_CONDITIONS_AND_EFFECTS") #
         # supported_kind.set_time("EXTERNAL_CONDITIONS_AND_EFFECTS")
         # supported_kind.set_time("TIMED_EFFECTS") #
         # supported_kind.set_time("TIMED_GOALS") #
-        # supported_kind.set_time("DURATION_INEQUALITIES")
+        supported_kind.set_time("DURATION_INEQUALITIES")
         # supported_kind.set_time("SELF_OVERLAPPING") #
         # supported_kind.set_time("INTERMEDIATE_CONDITIONS_AND_EFFECTS") #
-        # supported_kind.set_expression_duration("INT_TYPE_DURATIONS")
-        # supported_kind.set_expression_duration("REAL_TYPE_DURATIONS")
-        # supported_kind.set_expression_duration("STATIC_FLUENTS_IN_DURATIONS")
-        # supported_kind.set_expression_duration("FLUENTS_IN_DURATIONS")
-        # supported_kind.set_quality_metrics("ACTIONS_COST")
-        # supported_kind.set_actions_cost_kind("STATIC_FLUENTS_IN_ACTIONS_COST")
-        # supported_kind.set_actions_cost_kind("FLUENTS_IN_ACTIONS_COST")
-        # supported_kind.set_quality_metrics("FINAL_VALUE")
-        # supported_kind.set_quality_metrics("MAKESPAN")
-        # supported_kind.set_quality_metrics("PLAN_LENGTH")
-        # supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
-        # supported_kind.set_quality_metrics("TEMPORAL_OVERSUBSCRIPTION")
-        # supported_kind.set_actions_cost_kind("INT_NUMBERS_IN_ACTIONS_COST")
-        # supported_kind.set_actions_cost_kind("REAL_NUMBERS_IN_ACTIONS_COST")
-        # supported_kind.set_oversubscription_kind("INT_NUMBERS_IN_OVERSUBSCRIPTION")
-        # supported_kind.set_oversubscription_kind("REAL_NUMBERS_IN_OVERSUBSCRIPTION")
-        # supported_kind.set_simulated_entities("SIMULATED_EFFECTS")
-        # supported_kind.set_constraints_kind("STATE_INVARIANTS")
-        # supported_kind.set_constraints_kind("TRAJECTORY_CONSTRAINTS")
+        supported_kind.set_expression_duration("INT_TYPE_DURATIONS")
+        supported_kind.set_expression_duration("REAL_TYPE_DURATIONS")
+        supported_kind.set_expression_duration("STATIC_FLUENTS_IN_DURATIONS")
+        supported_kind.set_expression_duration("FLUENTS_IN_DURATIONS")
+        supported_kind.set_quality_metrics("ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("STATIC_FLUENTS_IN_ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("FLUENTS_IN_ACTIONS_COST")
+        supported_kind.set_quality_metrics("FINAL_VALUE")
+        supported_kind.set_quality_metrics("MAKESPAN")
+        supported_kind.set_quality_metrics("PLAN_LENGTH")
+        supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
+        supported_kind.set_quality_metrics("TEMPORAL_OVERSUBSCRIPTION")
+        supported_kind.set_actions_cost_kind("INT_NUMBERS_IN_ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("REAL_NUMBERS_IN_ACTIONS_COST")
+        supported_kind.set_oversubscription_kind("INT_NUMBERS_IN_OVERSUBSCRIPTION")
+        supported_kind.set_oversubscription_kind("REAL_NUMBERS_IN_OVERSUBSCRIPTION")
+        supported_kind.set_simulated_entities("SIMULATED_EFFECTS")
+        supported_kind.set_constraints_kind("STATE_INVARIANTS")
+        supported_kind.set_constraints_kind("TRAJECTORY_CONSTRAINTS")
         return supported_kind
 
     @staticmethod
@@ -142,12 +123,7 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
         problem_kind: ProblemKind, compilation_kind: Optional[CompilationKind] = None
     ) -> ProblemKind:
         new_kind = problem_kind.clone()
-        if new_kind.has_object_fluents():
-            new_kind.unset_fluents_type("OBJECT_FLUENTS")
-            new_kind.set_effects_kind("CONDITIONAL_EFFECTS")
-            new_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
-            new_kind.set_conditions_kind("EQUALITIES")
-            new_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
+        raise NotImplementedError
         return new_kind
 
     def _compile(
@@ -159,17 +135,61 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
         TODO
         """
         assert isinstance(problem, Problem)
-        raise NotImplementedError
+
+        # if problem is already sequential return problem? or raise
+
+        # TODO conditions at start -> preconditions copypaste
+        # TODO conditions during and at end -> preconditions but substitute with start effects dict
+        # TODO effects:
+        # > effects_start \ effects_end (remove from start_effects effects on fluents that are also modified at end)
+        # > effects_end but substitute with start_effects_dict on values
+        # TODO start effects dict -> start effects transition encoded in dict that can be used by substituter walker
         env = problem.environment
         tm = env.type_manager
         em = env.expression_manager
 
         new_to_old: Dict[Action, Optional[Action]] = {}
 
-        new_problem = Problem(f"{self.name}_{problem.name}", env)
-        new_problem.add_objects(problem.all_objects)
+        new_problem = problem.clone()
+        new_problem.name = f"{self.name}_{problem.name}"
 
-        fluents_map: Dict[Fluent, Fluent] = {}
+        new_problem.clear_actions()
+
+        for action in problem.actions:
+            new_action = InstantaneousAction(action.name)
+            assert isinstance(action, DurativeAction)
+            start = StartTiming()
+            end = EndTiming()
+            old_end_effects: Dict = {}
+            old_start_effects: Dict = {}  # NOTE also used for substitution
+            for i, oel in action.effects.items():
+                # TODO FIXME this probably does not work with non assignment effects
+                if i == start:
+                    for oe in oel:
+                        old_start_effects[oe.fluent] = oe.value
+                elif i == end:
+                    for oe in oel:
+                        old_end_effects[oe.fluent] = oe.value
+                else:
+                    raise BaseException
+
+            for i, ocl in action.conditions.items():
+                if i.upper == start and i.lower == start:
+                    for oc in ocl:
+                        new_action.add_precondition(oc)
+                else:
+                    for oc in ocl:
+                        new_action.add_precondition(oc.substitute(old_start_effects))
+
+            for oeef, oeev in old_end_effects.items():
+                new_v = oeev.substitute(old_start_effects)
+                new_action.add_effect(oeef, new_v)
+            for osef, osev in old_start_effects.items():
+                if osef not in old_end_effects.keys():
+                    new_action.add_effect(osef, osev)
+
+            # other things like simulated effects ... TODO
+            new_problem.add_action(new_action)
 
         return CompilerResult(
             new_problem, partial(replace_action, map=new_to_old), self.name
---
+++ unified_planning/test/test_t2s.py
@@ -0,0 +1,86 @@
+# Copyright 2021-2023 AIPlan4EU project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from fractions import Fraction
+import os
+from unified_planning.plans import ActionInstance
+import unified_planning
+from unified_planning.environment import get_environment
+from unified_planning.shortcuts import *
+from unified_planning.test import (
+    unittest_TestCase,
+    main,
+    skipIfNoPlanValidatorForProblemKind,
+    skipIfNoOneshotPlannerForProblemKind,
+)
+from unified_planning.test.examples import get_example_problems
+from unified_planning.model.problem_kind import (
+    basic_classical_kind,
+    classical_kind,
+    basic_temporal_kind,
+    full_classical_kind,
+)
+from unified_planning.engines.compilers import NegativeConditionsRemover
+from unified_planning.engines import SequentialPlanValidator as PV, CompilationKind
+from unified_planning.exceptions import (
+    UPExpressionDefinitionError,
+    UPProblemDefinitionError,
+)
+from unified_planning.model.problem_kind_versioning import LATEST_PROBLEM_KIND_VERSION
+
+from unified_planning.model.problem_kind import ProblemKind
+
+from unified_planning.engines.compilers.timed_to_sequential import TimedToSequential
+
+
+class TestT2S(unittest_TestCase):
+    def setUp(self):
+        unittest_TestCase.setUp(self)
+        self.environment = get_environment()
+        self.problems = get_example_problems()
+
+    def test_base_example(self):
+        problem = Problem("wompwomp")
+
+        x = Fluent("x", IntType())
+        problem.add_fluent(x)
+        problem.set_initial_value(x, 1)
+
+        y = Fluent("y", IntType())
+        problem.add_fluent(y)
+        problem.set_initial_value(y, 1)
+
+        z = Fluent("z", IntType())
+        problem.add_fluent(z)
+        problem.set_initial_value(z, 1)
+
+        tda = DurativeAction("tda")
+        tda.set_closed_duration_interval(5, 10)
+        tda.add_effect(StartTiming(), x, x + 1)
+        tda.add_effect(EndTiming(), x, x + 1)
+        tda.add_effect(StartTiming(), y, y + 1)
+        tda.add_effect(EndTiming(), z, y + 1)
+        tda.add_condition(StartTiming(), Equals(x, 1))
+        tda.add_condition(EndTiming(), Not(Equals(y, 1)))
+
+        # start 1, 1, 1
+        # during 2, 2, 1
+        # end 3, 2, 3
+
+        problem.add_action(tda)
+
+        t2s = TimedToSequential()
+        comp_res = t2s.compile(problem, CompilationKind.TIMED_TO_SEQUENTIAL)
+        self.assertTrue(not comp_res.problem.kind.has_continuous_time())
