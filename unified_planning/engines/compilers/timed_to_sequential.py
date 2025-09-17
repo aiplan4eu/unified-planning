cb96b4c1c43cc7e5a7165cebfd10931bf2536b19
Samuel Gobbi <sgobbi@fbk.eu>
2025-09-17 09:50:52 +0200

    refactor(t2s): removed effects data structures extraction from _compile and use partial for plan back conversion

--- unified_planning/engines/compilers/timed_to_sequential.py
+++ unified_planning/engines/compilers/timed_to_sequential.py
@@ -27,6 +27,9 @@ from unified_planning.model import (
     Effect,
     State,
     UPState,
+    ExpressionManager,
+    Fluent,
+    FNode,
 )
 from unified_planning.model.timing import StartTiming, EndTiming, Interval
 from unified_planning.model.problem_kind_versioning import LATEST_PROBLEM_KIND_VERSION
@@ -49,6 +52,74 @@ from unified_planning.plans import (
 )
 from unified_planning.model.problem_kind import FEATURES
 from unified_planning.engines import UPSequentialSimulator
+
+
+def plan_back_conversion_callable(
+    sp: SequentialPlan, problem: Problem, new_problem: Problem, new_to_old: Dict
+):
+    if not isinstance(sp, SequentialPlan):
+        raise UPUsageError("Plan to map back is not sequential")
+    if problem.epsilon is not None:
+        min_time_step = problem.epsilon
+    else:
+        min_time_step = Fraction(1, 100)
+    simulator = UPSequentialSimulator(new_problem)
+    simplifier = Simplifier(problem.environment)
+    fve = FreeVarsExtractor()
+    state: Optional[State] = simulator.get_initial_state()
+    assert isinstance(state, UPState)
+    time_now = Fraction(0)
+    ttptuples: List[Tuple[Fraction, ActionInstance, Optional[Fraction]]] = []
+    for action_instance in sp.actions:
+        if action_instance.action not in new_to_old:
+            raise UPUsageError(
+                f"Action {action_instance.action.name} not found during compilation is present in this plan"
+            )
+        action_for_mapback = new_to_old[action_instance.action]
+        assert action_for_mapback is not None
+        new_action_instance = ActionInstance(
+            action=action_for_mapback,
+            params=action_instance.actual_parameters,
+            agent=action_instance.agent,
+            motion_paths=action_instance.motion_paths,
+        )
+        if isinstance(action_for_mapback, DurativeAction):
+            tinterval = action_for_mapback.duration
+            assert isinstance(tinterval, Interval)
+            dtime = min_time_step
+            if not tinterval.is_left_open():
+                if tinterval.lower.is_constant():
+                    dtime = Fraction(tinterval.lower.constant_value())
+                else:
+                    par_sub_dict: Dict = {}
+                    for paramname, paramvalue in zip(
+                        action_for_mapback.parameters,
+                        action_instance.actual_parameters,
+                    ):
+                        par_sub_dict[paramname] = paramvalue
+                    tlower_with_pars = tinterval.lower.substitute(par_sub_dict)
+                    flu_subs_dict: Dict = {}
+                    for flu_obj in fve.get(tlower_with_pars):
+                        flu_subs_dict[flu_obj] = state.get_value(flu_obj)
+                    tlower_constant = simplifier.simplify(
+                        tlower_with_pars.substitute(flu_subs_dict)
+                    )
+                    dtime = Fraction(tlower_constant.constant_value())
+            else:
+                # NOTE if open use min step
+                dtime = min_time_step
+            ttptuples.append((time_now, new_action_instance, dtime))
+            time_now = time_now + dtime + min_time_step
+        elif isinstance(action_for_mapback, InstantaneousAction):
+            ttptuples.append((time_now, new_action_instance, None))
+            time_now = time_now + min_time_step
+        else:
+            # NOTE this should not happen as other kinds of transitions are not supported
+            raise UPUsageError
+        state = simulator.apply(cast(State, state), action_instance)
+        assert state is not None
+    ttp = TimeTriggeredPlan(ttptuples)
+    return ttp
 
 
 class TimedToSequential(engines.engine.Engine, CompilerMixin):
@@ -137,6 +208,89 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
             new_kind.unset_expression_duration(durfeat)
         return new_kind
 
+    def get_effects_data_structures(
+        self, action: DurativeAction, em: ExpressionManager, fve: FreeVarsExtractor
+    ) -> Tuple[Dict, Dict[Fluent, List[Effect]], Dict[Fluent, List[Effect]]]:
+        old_end_effects: Dict = {}
+        old_start_effects: Dict = {}
+        for timepoint, oel in action.effects.items():
+            if timepoint == StartTiming():
+                for oe in oel:
+                    if oe.fluent not in old_start_effects.keys():
+                        old_start_effects[oe.fluent] = []
+                    old_start_effects[oe.fluent].append(oe)
+            elif timepoint == EndTiming():
+                for oe in oel:
+                    if oe.fluent not in old_end_effects.keys():
+                        old_end_effects[oe.fluent] = []
+                    old_end_effects[oe.fluent].append(oe)
+            else:
+                raise UPUnsupportedProblemTypeError(
+                    "Intermediate effects are not supported"
+                )
+        start_effects_subs = self.get_start_effects_substitutions(
+            action=action,
+            old_start_effects=old_start_effects,
+            old_end_effects=old_end_effects,
+            fve=fve,
+            em=em,
+        )
+        return start_effects_subs, old_start_effects, old_end_effects
+
+    def get_start_effects_substitutions(
+        self,
+        action: DurativeAction,
+        old_start_effects: Dict[Fluent, List[Effect]],
+        old_end_effects: Dict[Fluent, List[Effect]],
+        fve: FreeVarsExtractor,
+        em: ExpressionManager,
+    ):
+        start_effects_subs: Dict = {}
+        for osef, osel in old_start_effects.items():
+            start_effects_subs[osef] = osef
+            for ose in osel:
+                assert isinstance(ose, Effect)
+                if not ose.condition == em.TRUE():
+                    dangerous_fluent = ose.fluent
+                    for tinterval, ocl in action.conditions.items():
+                        if tinterval.upper == EndTiming():
+                            # postconds and invariants (time upper = end)
+                            for oc in ocl:
+                                if dangerous_fluent in fve.get(oc):
+                                    raise UPUnsupportedProblemTypeError(
+                                        "start time conditional effects that affect fluents used in invariants/post conditions are not supported"
+                                    )
+                    for oeel in old_end_effects.values():
+                        for oee in oeel:
+                            if oee.is_increase() or oee.is_decrease():
+                                # fluent and value sides of inc/dec posteffs
+                                if dangerous_fluent in fve.get(
+                                    oee.fluent
+                                ) or dangerous_fluent in fve.get(oee.value):
+                                    raise UPUnsupportedProblemTypeError(
+                                        "start time conditional effects that affect increase/descrease effects at end time are not supported"
+                                    )
+                            else:
+                                # value side of assign posteffs
+                                if dangerous_fluent in fve.get(oee.value):
+                                    raise UPUnsupportedProblemTypeError(
+                                        "start time conditional effects that affect values of effects at end time are not supported"
+                                    )
+                if ose.is_assignment():
+                    # NOTE we should never find assignments associated with any other kind of effect
+                    start_effects_subs[ose.fluent] = ose.value
+                elif ose.is_increase():
+                    start_effects_subs[ose.fluent] = em.Plus(
+                        start_effects_subs[ose.fluent], ose.value
+                    )
+                elif ose.is_decrease():
+                    start_effects_subs[ose.fluent] = em.Minus(
+                        start_effects_subs[ose.fluent], ose.value
+                    )
+                else:
+                    raise UPUnreachableCodeError
+        return start_effects_subs
+
     def _compile(
         self,
         problem: "up.model.AbstractProblem",
@@ -174,70 +328,12 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
                 pdict[p.name] = p.type
             new_action = InstantaneousAction(action.name, pdict)
             assert isinstance(action, DurativeAction)
-            start = StartTiming()
-            end = EndTiming()
-            old_end_effects: Dict = {}
-            old_start_effects: Dict = {}
-            for timepoint, oel in action.effects.items():
-                if timepoint == start:
-                    for oe in oel:
-                        if oe.fluent not in old_start_effects.keys():
-                            old_start_effects[oe.fluent] = []
-                        old_start_effects[oe.fluent].append(oe)
-                elif timepoint == end:
-                    for oe in oel:
-                        if oe.fluent not in old_end_effects.keys():
-                            old_end_effects[oe.fluent] = []
-                        old_end_effects[oe.fluent].append(oe)
-                else:
-                    raise UPUnsupportedProblemTypeError(
-                        "Intermediate effects are not supported"
-                    )
 
-            subs_dict: Dict = {}
-            for osef, osel in old_start_effects.items():
-                subs_dict[osef] = osef
-                for ose in osel:
-                    assert isinstance(ose, Effect)
-                    if not ose.condition == em.TRUE():
-                        dangerous_fluent = ose.fluent
-                        for tinterval, ocl in action.conditions.items():
-                            if tinterval.upper == end:
-                                # postconds and invariants (time upper = end)
-                                for oc in ocl:
-                                    if dangerous_fluent in fve.get(oc):
-                                        raise UPUnsupportedProblemTypeError(
-                                            "start time conditional effects that affect fluents used in invariants/post conditions are not supported"
-                                        )
-                        for oeel in old_end_effects.values():
-                            for oee in oeel:
-                                if oee.is_increase() or oee.is_decrease():
-                                    # fluent and value sides of inc/dec posteffs
-                                    if dangerous_fluent in fve.get(
-                                        oee.fluent
-                                    ) or dangerous_fluent in fve.get(oee.value):
-                                        raise UPUnsupportedProblemTypeError(
-                                            "start time conditional effects that affect increase/descrease effects at end time are not supported"
-                                        )
-                                else:
-                                    # value side of assign posteffs
-                                    if dangerous_fluent in fve.get(oee.value):
-                                        raise UPUnsupportedProblemTypeError(
-                                            "start time conditional effects that affect values of effects at end time are not supported"
-                                        )
-                    if ose.is_assignment():
-                        # NOTE we should never find assignments associated with any other kind of effect
-                        subs_dict[ose.fluent] = ose.value
-                    elif ose.is_increase():
-                        subs_dict[ose.fluent] = em.Plus(
-                            subs_dict[ose.fluent], ose.value
-                        )
-                    elif ose.is_decrease():
-                        subs_dict[ose.fluent] = em.Minus(
-                            subs_dict[ose.fluent], ose.value
-                        )
-                    else:
-                        raise UPUnreachableCodeError
+            (
+                start_effects_subs,
+                old_start_effects,
+                old_end_effects,
+            ) = self.get_effects_data_structures(action=action, em=em, fve=fve)
 
             for timeinterval, ocl in action.conditions.items():
                 # intermediate not supported
@@ -245,30 +341,30 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
                 # upper = lower = end -> postcond (must sub)
                 # upper = end, lower = start -> invariant (both sub and not)
                 # so basically, if lower = start then add condition without sub, if upper = end then add condition with sub
-                if timeinterval.lower == start:
+                if timeinterval.lower == StartTiming():
                     for oc in ocl:
                         new_action.add_precondition(oc)
-                if timeinterval.upper == end:
+                if timeinterval.upper == EndTiming():
                     for oc in ocl:
-                        new_action.add_precondition(oc.substitute(subs_dict))
+                        new_action.add_precondition(oc.substitute(start_effects_subs))
 
             for oeef, oeel in old_end_effects.items():
                 for oee in oeel:
                     assert isinstance(oee, Effect)
                     if not oee.condition == em.TRUE():
-                        new_cond = oee.condition.substitute(subs_dict)
+                        new_cond = oee.condition.substitute(start_effects_subs)
                     else:
                         new_cond = em.TRUE()
                     if oee.is_assignment():
-                        new_value = oee.value.substitute(subs_dict)
+                        new_value = oee.value.substitute(start_effects_subs)
                         new_action.add_effect(oeef, new_value, new_cond)
                     elif oee.is_increase():
                         new_value = em.Plus(oeef, oee.value)
-                        new_value = new_value.substitute(subs_dict)
+                        new_value = new_value.substitute(start_effects_subs)
                         new_action.add_effect(oeef, new_value, new_cond)
                     elif oee.is_decrease():
                         new_value = em.Minus(oeef, oee.value)
-                        new_value = new_value.substitute(subs_dict)
+                        new_value = new_value.substitute(start_effects_subs)
                         new_action.add_effect(oeef, new_value, new_cond)
                     else:
                         raise UPUnreachableCodeError
@@ -292,73 +388,14 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
             new_to_old[new_action] = action
             new_problem.add_action(new_action)
 
-        def map_back_callable(sp: Plan) -> Plan:
-            if not isinstance(sp, SequentialPlan):
-                raise UPUsageError("Plan to map back is not sequential")
-            if problem.epsilon is not None:
-                min_time_step = problem.epsilon
-            else:
-                min_time_step = Fraction(1, 100)
-            simulator = UPSequentialSimulator(new_problem)
-            simplifier = Simplifier(problem.environment)
-            state: Optional[State] = simulator.get_initial_state()
-            assert isinstance(state, UPState)
-            time_now = Fraction(0)
-            ttptuples: List[Tuple[Fraction, ActionInstance, Optional[Fraction]]] = []
-            for action_instance in sp.actions:
-                if action_instance.action not in new_to_old:
-                    raise UPUsageError(
-                        f"Action {action_instance.action.name} not found during compilation is present in this plan"
-                    )
-                action_for_mapback = new_to_old[action_instance.action]
-                assert action_for_mapback is not None
-                new_action_instance = ActionInstance(
-                    action=action_for_mapback,
-                    params=action_instance.actual_parameters,
-                    agent=action_instance.agent,
-                    motion_paths=action_instance.motion_paths,
-                )
-                if isinstance(action_for_mapback, DurativeAction):
-                    tinterval = action_for_mapback.duration
-                    assert isinstance(tinterval, Interval)
-                    dtime = min_time_step
-                    if not tinterval.is_left_open():
-                        if tinterval.lower.is_constant():
-                            dtime = Fraction(tinterval.lower.constant_value())
-                        else:
-                            par_sub_dict: Dict = {}
-                            for paramname, paramvalue in zip(
-                                action_for_mapback.parameters,
-                                action_instance.actual_parameters,
-                            ):
-                                par_sub_dict[paramname] = paramvalue
-                            tlower_with_pars = tinterval.lower.substitute(par_sub_dict)
-                            flu_subs_dict: Dict = {}
-                            for flu_obj in fve.get(tlower_with_pars):
-                                flu_subs_dict[flu_obj] = state.get_value(flu_obj)
-                            tlower_constant = simplifier.simplify(
-                                tlower_with_pars.substitute(flu_subs_dict)
-                            )
-                            dtime = Fraction(tlower_constant.constant_value())
-                    else:
-                        # NOTE if open use min step
-                        dtime = min_time_step
-                    ttptuples.append((time_now, new_action_instance, dtime))
-                    time_now = time_now + dtime + min_time_step
-                elif isinstance(action_for_mapback, InstantaneousAction):
-                    ttptuples.append((time_now, new_action_instance, None))
-                    time_now = time_now + min_time_step
-                else:
-                    # NOTE this should not happen as other kinds of transitions are not supported
-                    raise UPUsageError
-                state = simulator.apply(cast(State, state), action_instance)
-                assert state is not None
-            ttp = TimeTriggeredPlan(ttptuples)
-            return ttp
-
         return CompilerResult(
             problem=new_problem,
-            plan_back_conversion=map_back_callable,
+            plan_back_conversion=partial(
+                plan_back_conversion_callable,
+                problem=problem,
+                new_problem=new_problem,
+                new_to_old=new_to_old,
+            ),
             engine_name=self.name,
             map_back_action_instance=None,
         )
