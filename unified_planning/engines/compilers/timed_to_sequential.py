5603d0175f127cdfac96d61f2a26c9415df7ef21
Samuel <piggy2204@gmail.com>
2025-09-23 16:47:58 +0200

    chore: form

--- unified_planning/engines/compilers/timed_to_sequential.py
+++ unified_planning/engines/compilers/timed_to_sequential.py
@@ -360,7 +360,7 @@ class TimedToSequential(engines.engine.Engine, CompilerMixin):
                         new_cond = em.TRUE()
                     if oee.is_assignment():
                         new_value = oee.value.substitute(start_effects_subs)
-                        if (new_value.is_bool_constant()):
+                        if new_value.is_bool_constant():
                             if (
                                 new_value.is_true() and oeef in new_action.preconditions
                             ) or (
