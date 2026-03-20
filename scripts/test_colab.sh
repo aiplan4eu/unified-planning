07288dbb883c66e3d9233425e320f2500699452e
Davide Lusuardi <lusuardi@fbk.eu>
2026-03-20 09:49:19 +0100

    fix(tamp): fix task-and-motion planning notebook

--- scripts/test_colab.sh
+++ scripts/test_colab.sh
@@ -8,13 +8,14 @@ for colab_file in `find ./docs/notebooks -name '*.ipynb'` ; do
     file_name="${colab_file%.ipynb}"
     python_file="${file_name}.py"
     python_basename_file=$(basename ${python_file})
+    file_dir=$(dirname "${python_file}")
     jupyter nbconvert --to python ${colab_file} --output ${python_basename_file} --TagRemovePreprocessor.remove_cell_tags remove_from_CI
     if [ ! $? == 0 ]; then
         echo " FAILED ${colab_file} conversion to python"
         exit 1
     fi
     echo -n "  -> Testing ${python_basename_file}..."
-    output=`ipython3 ${python_file} 2>&1`
+    output=$(cd "${file_dir}" && ipython3 "${python_basename_file}" 2>&1)
     if [ ! $? == 0 ]; then
         rm ${python_file}
         echo " FAILED for the following reason:"
